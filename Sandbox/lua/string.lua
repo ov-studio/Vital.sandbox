c3695d9f1baacc95e222db5b52f5f5fbd1d86093
Tron <48655846+OvileAmriam@users.noreply.github.com>
2022-09-15 00:19:04 +0300

    Update string.lua

--- Sandbox/lua/string.lua
+++ Sandbox/lua/string.lua
@@ -36,6 +36,7 @@ local string = class:create("string", utf8)
 for i, j in imports.pairs(imports.string) do
     string.public[i] = (not string.public[i] and j) or string.public[i]
 end
+utf8 = nil
 string.private.minifier = imports.md5("vStudio")
 
 function string.public.isVoid(baseString)
