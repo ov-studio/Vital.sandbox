514926d4db59c017ba7da290c07b9f37c66f9447
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-09-17 01:42:00 +0300

    Corrected file name

--- System/private/vital.cpp
+++ System/private/vital.cpp
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------
      Resource: Vital.sandbox
-     Script: System: public: vital.h
+     Script: System: public: vital.cpp
      Author: vStudio
      Developer(s): Aviril, Tron, Mario, Аниса
      DOC: 14/09/2022
