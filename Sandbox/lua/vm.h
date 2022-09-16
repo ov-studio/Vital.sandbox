7ef125b3e7b7f35a2993e628deedf6946e4aacde
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-09-16 16:20:38 +0300

    Update vm.h

--- Sandbox/lua/vm.h
+++ Sandbox/lua/vm.h
@@ -152,8 +152,8 @@ namespace Vital::Lua {
 
     // Method Binders
     static const bool bind(std::string parent, std::string name, vital_exec& exec) {
-        if (vMethodRefs[ref] && (vMethodRefs[ref] == exec)) return false;
         const std::string ref = parent + name;
+        if (vMethodRefs[ref] && (vMethodRefs[ref] == exec)) return false;
         vMethods.insert_or_assign(ref, [](luastate* vm) {
             exec(vInstances[vm]);
         });
