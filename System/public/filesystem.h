dafe9fc466506f0c139dd45af63906e2a3a2f652
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-09-17 00:54:19 +0300

    Used .h for only declarations

--- System/public/filesystem.h
+++ System/public/filesystem.h
@@ -25,41 +25,9 @@
 ////////////////////////////
 
 namespace Vital::FileSystem {
-    static const bool resolve(std::string& path) {
-        std::filesystem::path __path = std::filesystem::absolute(path);
-        if (!path.empty()) path = __path.string();
-        return true
-    }
-
-    static const bool exists(std::string& path) {
-        std::fstream handle(path, std::ios::in | std::ios::binary);
-        const bool isValid = (handle.is_open() && true) || false;
-        handle.close();
-        return isValid;
-    }
-
-    static const std::streampos size(std::fstream handle) {
-        if (!handle.is_open()) return 0;
-        handle.seekg(0, std::ios::end);
-        return handle.tellg();
-    }
-
-    static const char* read(std::string& path) {
-        std::fstream handle(path, std::ios::in | std::ios::binary | std::ios::ate);
-        char* buffer = nullptr;
-        if (handle.is_open()) {
-            const std::streampos size = handle.tellg();
-            char* buffer = new char[size];
-            handle.seekg(0, std::ios::beg);
-            handle.read(buffer, size);
-        }
-        return buffer;
-    }
-
-    static const bool write(std::string& path, char* buffer, std::streampos size) {
-        std::fstream handle(path, std::ios::out | std::ios::binary | std::ios::trunc);
-        handle.write(buffer, size);
-        handle.close();
-        return true;
-    }
+    static const bool resolve(std::string& path);
+    static const bool exists(std::string& path);
+    static const std::streampos size(std::fstream handle);
+    static const char* read(std::string& path);
+    static const bool write(std::string& path, char* buffer, std::streampos size);
 }
