16b0547e64712d5d676a91264ae46a9e17ee52de
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-17 23:29:40 +0300

    WIP virtual envs

--- Sandbox/lua/modules/resource.lua
+++ Sandbox/lua/modules/resource.lua
@@ -14,7 +14,8 @@
 
 local imports = {
     type = type,
-    pairs = pairs
+    pairs = pairs,
+    loadstring = loadstring
 }
 
 
@@ -45,11 +46,18 @@ end
 
 function resource.public:load(name)
     if not resource.public:isInstance(self) then return false end
-    self.rw = {}
+    self.rw = {
+        env = {}
+    }
     self.rw.manifest = table.decode(file.read("resources/"..name.."/manifest.vcl"))
     if not self.rw.manifest or not self.rw.manifest.scripts or (imports.type(self.rw.manifest.scripts) ~= "table") then return false end
     for i, j in imports.pairs(self.rw.manifest.scripts) do
-        --TODO: LOAD ALL SCRIPTS IN NEW ENV
+        local scriptData = file.read(self.rw.manifest.scripts)
+        if not scriptData then return false end
+        scriptData = imports.loadstring(scriptData)
+        --TODO: SET ENV
+        local status, error = assetify.imports.pcall(scriptData)
+        if not status then return false end
     end
     return self
 end
