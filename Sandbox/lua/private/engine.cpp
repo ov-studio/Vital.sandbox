9f6bda4a3892718663f11ed45c50d6f84e71934f
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-18 01:18:01 +0300

    Exposed tick APIs

---
+++ Sandbox/lua/private/engine.cpp
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Sandbox: lua: private: engine.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса
+     DOC: 14/09/2022
+     Desc: Engine Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include "Sandbox/lua/index.h"
+
+
+//////////////////////
+// Lua: FileSystem //
+//////////////////////
+
+namespace Vital::Lua {
+    bind("engine", "getSystemTick", [](vital_vm* vm) int {
+        vm -> setInt(Vital::getSystemTick());
+        return 1;
+    });
+
+    bind("engine", "getApplicationTick", [](vital_vm* vm) int {
+        vm -> setInt(Vital::getSystemTick());
+        return 1;
+    });
+}
