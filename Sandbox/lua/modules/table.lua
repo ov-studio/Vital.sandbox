ad48fc496f40ae57499cdb42da8c7812642de6f2
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-18 00:52:02 +0300

    Update table.lua

--- Sandbox/lua/modules/table.lua
+++ Sandbox/lua/modules/table.lua
@@ -106,7 +106,10 @@ function table.private.inspect(baseTable, showHidden, limit, level, buffer, skip
     return table.public.concat(buffer)
 end
 function table.public.inspect(...) return table.private.inspect(table.public.unpack(table.public.pack(...), 3)) end 
-function table.public.print(...) return imports.print(table.public.inspect(...)) end
+
+function table.public.print(...)
+    return imports.print(table.public.inspect(...))
+end
 
 function table.public.keys(baseTable)
     if not baseTable or (imports.type(baseTable) ~= "table") then return false end
