9ec5454675e7defd6a608fe782377381fb899355
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-09-24 02:32:16 +0300

    Update Vital.bundler.cpp

--- Vital.bundler/Vital.bundler.cpp
+++ Vital.bundler/Vital.bundler.cpp
@@ -10,15 +10,20 @@ int main() {
     path += ".gitignore";
     std::cout << "\nCurrent Tick: " << Vital::getApplicationTick();
     std::cout << "\nResolved Path: " << path;
-    std::cout << "\nIs File Existing: " << Vital::FileSystem::exists(path);
+    std::cout << "\nIs File Existing: " << Vital::FileSystem::exists(path) << "\n";
+
     Vital::Lua::API::onErrorHandle([](std::string& error) -> void {
         std::cout << "\nR-ERROR | " << error;
     });
     Vital::Lua::API::boot();
     Vital::Lua::vital_vm* test = new Vital::Lua::create();
+
     //test->registerFunction("me", reinterpret_cast<Vital::Lua::vital_exec>(mefunc));
-    std::string testbuffer = "print('hello') print('Table: '..tostring(table)) print('File: '..tostring(file)) print('Tick: '..tostring(engine.getApplicationTick()))";
-    //std::string testbuffer = "print('me: '..tostring(me))";
-    test -> loadString(testbuffer);
+    std::string testbuffer = "print('System Tick: '..tostring(engine.getSystemTick()))";
+    test->loadString(testbuffer);
+    testbuffer = "print('Application Tick: '..tostring(engine.getApplicationTick()))";
+    test->loadString(testbuffer);
+    testbuffer = "print('Current Path: '..file.resolve('../'))";
+    test->loadString(testbuffer);
     return 0;
 }
