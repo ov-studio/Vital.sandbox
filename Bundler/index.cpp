9fdfaa8b3ac4f5e7362b75410855816c0a8c11b9
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-09-27 17:30:38 +0300

    Made bundler logic shared

--- Bundler/index.cpp
+++ Bundler/index.cpp
@@ -2,16 +2,13 @@
 #include <Sandbox/lua/public/index.h>
 #include <System/public/filesystem.h>
 
-#include <Sandbox/lua/module/bundle.h>
-
-const std::string vSignature = "12a816e2d0469bf62c87e82ac58d58134b7ca9dd";
+const std::string vSignature = "VitalSDK";
 void outputConsole(std::string message) { std::cout << "\nVital.bundler | " << message; }
-void genLuaPackage() {
-    std::string entryPoint = "Sandbox/lua/module/";
+void genPackage(std::string name, std::string entry, std::vector<std::string> modules) {
     std::string rwBuffer = "std::vector<std::string> buffer = {";
-    outputConsole("Packaging Lua...");
-    for (auto i : Vital::Lua::vModules) {
-        std::string path = entryPoint + i;
+    outputConsole("Packaging " + name + "...");
+    for (auto i : modules) {
+        std::string path = entry + i;
         if (!Vital::FileSystem::exists(path)) {
             std::string error = "Invalid File: " + path;
             outputConsole(error);
@@ -24,35 +21,12 @@ void genLuaPackage() {
         }
     }
     rwBuffer += "\n};";
-    std::string path = entryPoint + "bundle.h";
+    std::string path = entry + "bundle.h";
     Vital::FileSystem::write(path, rwBuffer);
-    outputConsole("Packaged Lua successfully!");
+    outputConsole("Packaged " + name + " successfully!");
 }
 
 int main() {
-    genLuaPackage();
-
-    /*
-    std::string path = "../";
-    Vital::FileSystem::resolve(path);
-    path += ".gitignore";
-    std::cout << "\nCurrent Tick: " << Vital::getApplicationTick();
-    std::cout << "\nResolved Path: " << path;
-    std::cout << "\nIs File Existing: " << Vital::FileSystem::exists(path) << "\n";
-
-    Vital::Lua::API::onErrorHandle([](std::string& error) -> void {
-        std::cout << "\nR-ERROR | " << error;
-    });
-    Vital::Lua::API::boot();
-    Vital::Lua::vital_vm* test = new Vital::Lua::create();
-
-    //test->registerFunction("me", reinterpret_cast<Vital::Lua::vital_exec>(mefunc));
-    std::string testbuffer = "print('System Tick: '..tostring(engine.getSystemTick()))";
-    test->loadString(testbuffer);
-    testbuffer = "print('Application Tick: '..tostring(engine.getApplicationTick()))";
-    test->loadString(testbuffer);
-    testbuffer = "print('Current Path: '..file.resolve('../'))";
-    test->loadString(testbuffer);
-    */
+    genPackage("Lua", "Sandbox/lua/module/", Vital::Lua::vModules);
     return 0;
 }
