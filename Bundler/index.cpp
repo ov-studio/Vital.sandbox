a9c38ab91cc5bb62d8e249eece0cce429dd2e6b3
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-09-27 17:21:26 +0300

    Added bundle signature

--- Bundler/index.cpp
+++ Bundler/index.cpp
@@ -1,17 +1,37 @@
-#pragma once
+﻿#pragma once
 #include <Sandbox/lua/public/index.h>
-
-#include <iostream>
-#include <System/public/vital.h>
 #include <System/public/filesystem.h>
-#include <Sandbox/lua/public/api.h>
 
+#include <Sandbox/lua/module/bundle.h>
+
+const std::string vSignature = "12a816e2d0469bf62c87e82ac58d58134b7ca9dd";
+void outputConsole(std::string message) { std::cout << "\nVital.bundler | " << message; }
+void genLuaPackage() {
+    std::string entryPoint = "Sandbox/lua/module/";
+    std::string rwBuffer = "std::vector<std::string> buffer = {";
+    outputConsole("Packaging Lua...");
+    for (auto i : Vital::Lua::vModules) {
+        std::string path = entryPoint + i;
+        if (!Vital::FileSystem::exists(path)) {
+            std::string error = "Invalid File: " + path;
+            outputConsole(error);
+            throw error;
+        }
+        else {
+            std::string buffer = Vital::FileSystem::read(path);
+            rwBuffer += "\nR\"" + vSignature + "(\n" + buffer + "\n)" + vSignature + "\",";
+            outputConsole("Bundled File: " + path);
+        }
+    }
+    rwBuffer += "\n};";
+    std::string path = entryPoint + "bundle.h";
+    Vital::FileSystem::write(path, rwBuffer);
+    outputConsole("Packaged Lua successfully!");
+}
 
 int main() {
-    std::cout << "Bundling : Lua";
-    for (auto i : Vital::Lua::vModules) {
-        std::cout << i;
-    }
+    genLuaPackage();
+
     /*
     std::string path = "../";
     Vital::FileSystem::resolve(path);
