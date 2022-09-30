80cff5b334eb4ee45c9476832fb711348450bb93
Tron <48655846+OvileAmriam@users.noreply.github.com>
2022-09-30 17:14:31 +0300

    Update crypto.lua

--- Sandbox/lua/module/crypto.lua
+++ Sandbox/lua/module/crypto.lua
@@ -23,24 +23,8 @@ local imports = {
 
 local crypto = class:create("crypto")
 
-function crypto.public:sha1(...)
-    return imports.crypto.sha1(...)
-end
-
-function crypto.public:sha224(...)
-    return imports.crypto.sha224(...)
-end
-
-function crypto.public:sha256(...)
-    return imports.crypto.sha256(...)
-end
-
-function crypto.public:sha384(...)
-    return imports.crypto.sha384(...)
-end
-
-function crypto.public:sha512(...)
-    return imports.crypto.sha512(...)
+function crypto.public:hash(...)
+    return imports.crypto.hash(...)
 end
 
 function crypto.public:encrypt(...)
