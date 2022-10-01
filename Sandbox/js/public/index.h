e6241cf48caecf57343226dd4bc3d406228d25c7
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-10-01 14:35:06 +0300

    Embedding duktape vm

---
+++ Sandbox/js/public/index.h
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Sandbox: js: public: index.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса
+     DOC: 14/09/2022
+     Desc: Sandbox Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <pch.h>
+#include <Vendor/duktape/duktape.h>
+
+
+////////////////////
+// Namespace: JS //
+////////////////////
+
+namespace Vital::JS {}
