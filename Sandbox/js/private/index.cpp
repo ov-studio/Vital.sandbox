3da346293bc05655704bd2ccddecf1d53dafcc2c
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-10-01 14:36:43 +0300

    Create index.cpp

---
+++ Sandbox/js/private/index.cpp
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Sandbox: js: public: index.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса
+     DOC: 14/09/2022
+     Desc: Sandbox Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Sandbox/js/public/index.h>
+
+
+////////////////////
+// Namespace: JS //
+////////////////////
+
+namespace Vital::JS {}
