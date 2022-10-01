7c2e63197821a1cde8afb39f09b98372489bbbea
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-10-01 15:36:46 +0300

    Exposing duktape feature to SDK

---
+++ Sandbox/js/private/vm.cpp
@@ -0,0 +1,182 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Sandbox: js: public: vm.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса
+     DOC: 14/09/2022
+     Desc: VM Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/filesystem.h>
+// TODO: REMOVE LATER
+#include <Sandbox/js/public/vm.h>
+//#include <Sandbox/js/public/api.h>
+#if __has_include(<Sandbox/js/module/bundle.h>)
+    #include <Sandbox/js/module/bundle.h>
+#endif
+
+
+/////////////
+// JS: VM //
+/////////////
+
+namespace Vital::JS {
+    // Instantiators //
+    std::map<vital_ref*, vital_vm*> vInstances;
+    vital_vm* fetchVM(vital_ref* vm) { return vInstances[vm]; }
+    create::create() {
+        vm = duk_create_heap_default();
+        vInstances.emplace(vm, this);
+        for (std::string i : vBlacklist) {
+            setNil();
+            setGlobal(i);
+        }
+        /*
+        for (auto& i : API::vMethods) {
+            registerFunction(i.first.second, i.second, i.first.first);
+        }
+        */
+        #if __has_include(<Sandbox/js/module/bundle.h>)
+            for (std::string i : vBundle) {
+                loadString(i);
+            }
+        #endif
+    }
+    bool create::destroy() {
+        if (isUnloaded) return false;
+        isUnloaded = true;
+        duk_destroy_heap(vm);
+        vm = nullptr;
+        return true;
+    }
+
+    // Checkers //
+    bool create::isNil(int index) { return lua_isnoneornil(vm, index); }
+    bool create::isBool(int index) { return lua_isboolean(vm, index); }
+    bool create::isString(int index) { return lua_isstring(vm, index); }
+    bool create::isNumber(int index) { return lua_isnumber(vm, index); }
+    bool create::isTable(int index) { return lua_istable(vm, index); }
+    bool create::isThread(int index) { return lua_isthread(vm, index); }
+    bool create::isUserData(int index) { return lua_isuserdata(vm, index); }
+    bool create::isFunction(int index) { return lua_isfunction(vm, index); }
+
+    // Setters //
+    void create::setGlobal(std::string index) { lua_setglobal(vm, index.data()); }
+    void create::setNil() { lua_pushnil(vm); }
+    void create::setBool(bool value) { lua_pushboolean(vm, static_cast<int>(value)); }
+    void create::setString(std::string& value) { lua_pushstring(vm, value.data()); }
+    void create::setInt(int value) { lua_pushnumber(vm, static_cast<lua_Number>(value)); }
+    void create::setFloat(float value) { lua_pushnumber(vm, static_cast<lua_Number>(value)); }
+    void create::setDouble(double value) { lua_pushnumber(vm, static_cast<lua_Number>(value)); }
+    void create::createTable() { lua_newtable(vm); }
+    void create::setTable(int index) { lua_settable(vm, index); }
+    void create::setTableField(int value, int index) { lua_seti(vm, index, value); }
+    void create::setTableField(std::string value, int index) { lua_setfield(vm, index, value.data()); }
+    void create::createMetaTable(std::string value) { luaL_newmetatable(vm, value.data()); }
+    void create::setMetaTable(int index) { lua_setmetatable(vm, index); }
+    void create::setMetaTable(std::string index) { luaL_setmetatable(vm, index.data()); }
+    void create::createUserData(void* value) {
+        void** userdata = static_cast<void**>(lua_newuserdata(vm, sizeof(void*)));
+        *userdata = value;
+        return;
+    }
+    void create::setUserData(void* value) { lua_pushlightuserdata(vm, value); }
+    void create::setFunction(vital_exec& value) { lua_pushcfunction(vm, value); }
+
+    // Getters //
+    int create::getArgCount() { return lua_gettop(vm); }
+    bool create::getGlobal(std::string index) { return lua_getglobal(vm, index.data()); }
+    bool create::getBool(int index) { return static_cast<bool>(lua_toboolean(vm, index)); }
+    std::string create::getString(int index) { return lua_tostring(vm, index); }
+    int create::getInt(int index) { return static_cast<int>(lua_tonumber(vm, index)); }
+    float create::getFloat(int index) { return static_cast<float>(lua_tonumber(vm, index)); }
+    double create::getDouble(int index) { return static_cast<double>(lua_tonumber(vm, index)); }
+    bool create::getTable(int index) { return lua_gettable(vm, index); }
+    bool create::getTableField(int value, int index) { return lua_geti(vm, index, value); }
+    bool create::getTableField(std::string value, int index) { return lua_getfield(vm, index, value.data()); }
+    bool create::getMetaTable(int index) { return lua_getmetatable(vm, index); }
+    void* create::getUserData(int index) { return lua_touserdata(vm, index); }
+    int create::getLength(int index) {
+        lua_len(vm, index);
+        int result = getInt(-1);
+        pop();
+        return result;
+    }
+
+    // Pushers //
+    void create::pushBool(bool value) {
+        setBool(value);
+        setTableField(getLength(-2) + 1, -2);
+    }
+    void create::pushString(std::string& value) {
+        setString(value);
+        setTableField(getLength(-2) + 1, -2);
+    }
+    void create::pushNumber(int value) {
+        setInt(value);
+        setTableField(getLength(-2) + 1, -2);
+    }
+    void create::pushFunction(vital_exec& exec) {
+        setFunction(exec);
+        setTableField(getLength(-2) + 1, -2);
+    }
+
+    // Registerers //
+    void create::registerBool(std::string index, bool value) {
+        setBool(value);
+        setTableField(index.data(), -2);
+    }
+    void create::registerString(std::string index, std::string& value) {
+        setString(value);
+        setTableField(index.data(), -2);
+    }
+    void create::registerNumber(std::string index, int value) {
+        setInt(value);
+        setTableField(index.data(), -2);
+    }
+    void create::registerFunction(std::string index, vital_exec& exec) {
+        setFunction(exec);
+        setTableField(index.data(), -2);
+    }
+    void create::registerFunction(std::string index, vital_exec& exec, std::string parent) {
+        getGlobal(parent);
+        if (!isTable(-1)) {
+            createTable();
+            setGlobal(parent);
+            getGlobal(parent);
+        }
+        registerFunction(index, exec);
+    }
+    void create::registerObject(std::string index, void* value) {
+        createUserData(value);
+        setMetaTable(index);
+    }
+
+    // Utils //
+    void create::pop(int count) { lua_pop(vm, count); }
+    bool create::loadString(std::string& buffer) {
+        if (buffer.empty()) return false;
+        luaL_loadstring(vm, buffer.data());
+        bool status = !lua_pcall(vm, 0, LUA_MULTRET, 0);
+        if (!status) {
+            std::string error = getString(-1);
+            API::error(error);
+            return false;
+        }
+        return true;
+    }
+    bool create::throwError(std::string& error) {
+        lua_Debug debug;
+        lua_getstack(vm, 1, &debug);
+        lua_getinfo(vm, "nSl", &debug);
+        error = "[ERROR - L" + std::to_string(debug.currentline) + "] | Reason: " + (error.empty() ? "N/A" : error);
+        API::error(error);
+        return true;
+    }
+}
