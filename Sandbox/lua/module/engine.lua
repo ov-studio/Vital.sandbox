248582b69e6aa30f30279622f9db4c31d4b274a1
Aviril <89867985+Aviril@users.noreply.github.com>
2022-10-01 12:27:59 +0300

    Update engine.lua

--- Sandbox/lua/module/engine.lua
+++ Sandbox/lua/module/engine.lua
@@ -28,32 +28,32 @@ engine.private.binds = {
     command = {}
 }
 
-function engine.private.isBindSource(type, source, ref, exec)
-    if not source or not ref or not type or not exec then return false end
-    if (imports.type(source) ~= "string") or (imports.type(ref) ~= "string") or (imports.type(type) ~= "string") or (imports.type(exec) ~= "function") then return false end
-    if not engine.private.binds[type] then return false end
+function engine.private.isBindSource(type, parent, ref, exec)
+    if not type or not parent or not ref or not exec then return false end
+    if (imports.type(type) ~= "string") or not engine.private.binds[type] then return false end
+    if (imports.type(parent) ~= "string") or (imports.type(ref) ~= "string") or (imports.type(exec) ~= "function") then return false end
     return true
 end
 
-function engine.private.bind(type, source, ref, exec)
-    if not engine.private.isBindSource(type, source, ref, exec) then return false end
+function engine.private.bind(type, parent, ref, exec)
+    if not engine.private.isBindSource(type, parent, ref, exec) then return false end
     engine.private.binds[type][ref] = engine.private.binds[type][ref] or {}
-    engine.private.binds[type][ref][source] = engine.private.binds[type][ref][source] or {}
+    engine.private.binds[type][ref][parent] = engine.private.binds[type][ref][parent] or {}
     if engine.private.binds[type][ref][exec] then return false end
     engine.private.binds[type][ref][exec] = true
     return true
 end
 
-function engine.private.unbind(type, source, ref, exec)
-    if not engine.private.isBindSource(type, source, ref, exec) then return false end
-    if not engine.private.binds[type][ref] or not engine.private.binds[type][ref][source] or not engine.private.binds[type][ref][exec] then return false end
+function engine.private.unbind(type, parent, ref, exec)
+    if not engine.private.isBindSource(type, parent, ref, exec) then return false end
+    if not engine.private.binds[type][ref] or not engine.private.binds[type][ref][parent] or not engine.private.binds[type][ref][exec] then return false end
     engine.private.binds[type][ref][exec] = nil
     return true
 end
 
 function engine.private.executeBind(type, ref)
 
-    if not ref or (imports.type(source) ~= "string") then return false end
+    if not ref or (imports.type(parent) ~= "string") then return false end
     if engine.private.binds[type][ref] then
         for i, j in imports.pairs(engine.private.binds[type][ref]) do
             for k, v in imports.pairs(j) do
@@ -62,7 +62,7 @@ function engine.private.executeBind(type, ref)
         end
     end
 
-    if not engine.private.binds[type][ref] or not engine.private.binds[type][ref][source] or not engine.private.binds[type][ref][exec] then return false end
+    if not engine.private.binds[type][ref] or not engine.private.binds[type][ref][parent] or not engine.private.binds[type][ref][exec] then return false end
     engine.private.binds[type][ref][exec] = nil
     return true
 end
