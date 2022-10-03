b34972a3036b3cbbb220d6b55dce1838369e214c
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-10-03 20:12:08 +0300

    Init audio system

--- System/private/audio.cpp
+++ System/private/audio.cpp
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------
      Resource: Vital.sandbox
-     Script: System: public: audio.h
+     Script: System: private: audio.cpp
      Author: vStudio
      Developer(s): Aviril, Tron, Mario, Аниса
      DOC: 14/09/2022
@@ -13,7 +13,9 @@
 //////////////
 
 #pragma once
-#include <System/public/vital.h>
+#include <System/public/audio.h>
+#include <Vendor/fmod/include/fmod.hpp>
+#include <Vendor/fmod/include/fmod_errors.h>
 
 
 ///////////////////////////
