7252dc063cc78f795791a8f21e522ed832fa0050
Aviril <89867985+Aviril@users.noreply.github.com>
2022-10-05 03:23:45 +0300

    Separated sound-spc

--- System/private/audio.cpp
+++ System/private/audio.cpp
@@ -63,98 +63,98 @@ namespace Vital::System::Audio {
         vSystem -> update();
         return true;
     }
+}
 
-    namespace Sound {
-        // Instantiators //
-        std::map<vital_sound*, bool> vInstances;
-        create::create(std::string& path) {
-            if (isErrored(vSystem -> createSound(path.data(), FMOD_DEFAULT, 0, &sound))) throw 0;
-            vInstances.emplace(this, true);
 
-            // TODO: REMOVE LATER
-            play();
-            setPitch(2);
-            do {
+namespace Vital::System::Audio::Sound {
+    // Instantiators //
+    std::map<vital_sound*, bool> vInstances;
+    create::create(std::string& path) {
+        if (isErrored(vSystem -> createSound(path.data(), FMOD_DEFAULT, 0, &sound))) throw 0;
+        vInstances.emplace(this, true);
+
+        // TODO: REMOVE LATER
+        play();
+        setPitch(2);
+        do {
                 
-            } while (true);
-        }
-        create::~create() {
-            if (!sound) return false;
-            vInstances.erase(this);
-            sound -> release();
-            sound = nullptr;
-            channel = nullptr;
-        }
+        } while (true);
+    }
+    create::~create() {
+        vInstances.erase(this);
+        sound -> release();
+        sound = nullptr;
+        channel = nullptr;
+    }
 
-        // Checkers //
-        bool create::isPlaying() {
-            bool state = false;
-            channel -> isPlaying(&state);
-            return state;
-        }
+    // Checkers //
+    bool create::isPlaying() {
+        bool state = false;
+        channel -> isPlaying(&state);
+        return state;
+    }
 
-        // Setters //
-        bool create::play() {
-            return !isErrored(vSystem -> playSound(sound, nullptr, false, &channel));
-        }
-        bool create::play(FMOD::ChannelGroup* channelGroup) {
-            return !isErrored(vSystem -> playSound(sound, channelGroup, false, &channel));
-        }
-        bool create::setChannelGroup(FMOD::ChannelGroup* channelGroup) {
-            return !isErrored(channel -> setChannelGroup(channelGroup));
-        }
-        bool create::stop() {
-            return !isErrored(channel -> stop());
-        }
-        bool create::setPaused(bool state) {
-            return !isErrored(channel -> setPaused(state));
-        }
-        bool create::setPitch(float value) {
-            return !isErrored(channel -> setPitch(value));
-        }
-        bool create::setVolume(float value) {
-            return !isErrored(channel -> setVolume(value));
-        }
-        bool create::setVolumeRamp(bool state) {
-            return !isErrored(channel -> setVolumeRamp(state));
-        }
-        bool create::setMute(bool state) {
-            return !isErrored(channel -> setMute(state));
-        }
-        bool create::setPan(float value) {
-            return !isErrored(channel -> setPan(value));
-        }
+    // Setters //
+    bool create::play() {
+        return !isErrored(vSystem -> playSound(sound, nullptr, false, &channel));
+    }
+    bool create::play(FMOD::ChannelGroup* channelGroup) {
+        return !isErrored(vSystem -> playSound(sound, channelGroup, false, &channel));
+    }
+    bool create::setChannelGroup(FMOD::ChannelGroup* channelGroup) {
+        return !isErrored(channel -> setChannelGroup(channelGroup));
+    }
+    bool create::stop() {
+        return !isErrored(channel -> stop());
+    }
+    bool create::setPaused(bool state) {
+        return !isErrored(channel -> setPaused(state));
+    }
+    bool create::setPitch(float value) {
+        return !isErrored(channel -> setPitch(value));
+    }
+    bool create::setVolume(float value) {
+        return !isErrored(channel -> setVolume(value));
+    }
+    bool create::setVolumeRamp(bool state) {
+        return !isErrored(channel -> setVolumeRamp(state));
+    }
+    bool create::setMute(bool state) {
+        return !isErrored(channel -> setMute(state));
+    }
+    bool create::setPan(float value) {
+        return !isErrored(channel -> setPan(value));
+    }
 
-        // Getters //
-        bool create::getPaused() {
-            bool state = false;
-            channel -> getPaused(&state);
-            return state;
-        }
-        float create::getPitch() {
-            float value = 0;
-            channel -> getPitch(&value);
-            return value;
-        }
-        float create::getAudibility() {
-            float value = 0;
-            channel -> getAudibility(&value);
-            return value;
-        }
-        float create::getVolume() {
-            float value = 0;
-            channel -> getVolume(&value);
-            return value;
-        }
-        bool create::getVolumeRamp() {
-            bool state = false;
-            channel -> getVolumeRamp(&state);
-            return state;
-        }
-        bool create::getMute() {
-            bool state = false;
-            channel -> getMute(&state);
-            return state;
-        }
+    // Getters //
+    bool create::getPaused() {
+        bool state = false;
+        channel -> getPaused(&state);
+        return state;
+    }
+    float create::getPitch() {
+        float value = 0;
+        channel -> getPitch(&value);
+        return value;
+    }
+    float create::getAudibility() {
+        float value = 0;
+        channel -> getAudibility(&value);
+        return value;
+    }
+    float create::getVolume() {
+        float value = 0;
+        channel -> getVolume(&value);
+        return value;
+    }
+    bool create::getVolumeRamp() {
+        bool state = false;
+        channel -> getVolumeRamp(&state);
+        return state;
+    }
+    bool create::getMute() {
+        bool state = false;
+        channel -> getMute(&state);
+        return state;
     }
 }
--- System/public/audio.h
+++ System/public/audio.h
@@ -27,40 +27,40 @@ namespace Vital::System::Audio {
     extern bool destroy();
     extern bool update();
     extern FMOD::Sound* play(std::string& path);
+}
 
-    namespace Sound {
-        class create {
-            private:
-                FMOD::Sound* sound = nullptr;
-                FMOD::Channel* channel = nullptr;
-            public:
-                // Instantiators //
-                create(std::string& path);
-                ~create();
+namespace Vital::System::Audio::Sound {
+    class create {
+        private:
+            FMOD::Sound* sound = nullptr;
+            FMOD::Channel* channel = nullptr;
+        public:
+            // Instantiators //
+            create(std::string& path);
+            ~create();
 
-                // Checkers //
-                bool isPlaying();
+            // Checkers //
+            bool isPlaying();
 
-                // Setters //
-                bool play();
-                bool play(FMOD::ChannelGroup* channelGroup);
-                bool setChannelGroup(FMOD::ChannelGroup* channelGroup);
-                bool stop();
-                bool setPaused(bool state);
-                bool setPitch(float value);
-                bool setVolume(float value);
-                bool setVolumeRamp(bool state);
-                bool setMute(bool state);
-                bool setPan(float value);
+            // Setters //
+            bool play();
+            bool play(FMOD::ChannelGroup* channelGroup);
+            bool setChannelGroup(FMOD::ChannelGroup* channelGroup);
+            bool stop();
+            bool setPaused(bool state);
+            bool setPitch(float value);
+            bool setVolume(float value);
+            bool setVolumeRamp(bool state);
+            bool setMute(bool state);
+            bool setPan(float value);
 
-                // Getters //
-                bool getPaused();
-                float getPitch();
-                float getAudibility();
-                float getVolume();
-                bool getVolumeRamp();
-                bool getMute();
-        };
-        typedef create vital_sound;
-    }
+            // Getters //
+            bool getPaused();
+            float getPitch();
+            float getAudibility();
+            float getVolume();
+            bool getVolumeRamp();
+            bool getMute();
+    };
+    typedef create vital_sound;
 }
