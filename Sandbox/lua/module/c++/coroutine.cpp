3f40c9fd990b161e02650862b815d26761be3e4e
Aviril <116158274+A-Variakojiene@users.noreply.github.com>
2022-10-19 15:59:43 +0300

    Update coroutine.cpp

--- Sandbox/lua/module/c++/coroutine.cpp
+++ Sandbox/lua/module/c++/coroutine.cpp
@@ -28,10 +28,7 @@ namespace Vital::Sandbox::Lua::API {
         bind("coroutine", "create", [](vital_ref* ref) -> int {
             auto vm = fetchVM(ref);
             return vm -> execute([&]() -> int {
-                std::cout << "\n CREATE COROUT - ARG COUNT: " << vm -> getArgCount();
-                //if ((vm -> getArgCount() < 1) || (!vm -> isString(1))) throw ErrorCode["invalid-arguments"];
-                //std::string path = vm -> getString(1);
-                //vm -> createThread();
+                vm -> createThread();
                 return 1;
             });
         });
