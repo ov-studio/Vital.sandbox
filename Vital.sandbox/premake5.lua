71b55f501c666914dc50109301822bae34b0feac
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 11:09:43 +0300

    Update premake5.lua

--- Vital.sandbox/premake5.lua
+++ Vital.sandbox/premake5.lua
@@ -5,6 +5,10 @@ project "Vital.sandbox"
     targetdir ("../.build/"..outputdir.."/%{prj.name}")
     objdir ("../.output/"..outputdir.."/%{prj.name}")
     includedirs { ".", "Vendor", "Vendor/openssl/include", "Vendor/fmod/include", "Vendor/lua", "Vendor/duktape" }
+    filter "configurations:Debug"
+        links { "Vendor/openssl/x64/Debug/libcrypto.lib", "Vendor/openssl/x64/Debug/libssl.lib", "Vendor/fmod/lib/x64/fmodL_vc.lib" }
+    filter "configurations:Release"
+        links { "Vendor/openssl/x64/Release/libcrypto.lib", "Vendor/openssl/x64/Release/libssl.lib", "Vendor/fmod/lib/x64/fmod_vc.lib" }
     files {
         "premake5.lua",
         "**.cpp",
