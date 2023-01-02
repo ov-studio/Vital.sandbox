9385197bf5b0cdac1debdf3669a1428efabaf73a
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 15:11:56 +0300

    Update premake5.lua

--- Vital.sandbox/premake5.lua
+++ Vital.sandbox/premake5.lua
@@ -6,16 +6,16 @@ project "Vital.sandbox"
     objdir ("../.output/"..outputdir.."/%{prj.name}")
     includedirs { ".", "Vendor", "Vendor/openssl/include", "Vendor/fmod/include", "Vendor/lua", "Vendor/duktape" }
     filter "configurations:Debug"
-        links { "Vendor/openssl/lib/x64/Debug/libcrypto.lib", "Vendor/openssl/lib/x64/Debug/libssl.lib", "Vendor/fmod/lib/x64/fmodL_vc.lib" }
+        links { "Vendor/openssl/lib/%{cfg.platform}/Debug/libcrypto.lib", "Vendor/openssl/lib/%{cfg.platform}/Debug/libssl.lib", "Vendor/fmod/lib/%{cfg.platform}/fmodL_vc.lib" }
         postbuildcommands {
-            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Debug\\ossl_static.pdb $(OutDir)",
-            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\x64\\fmodL_vc.lib $(OutDir)"
+            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\%{cfg.platform}\\Debug\\ossl_static.pdb $(OutDir)",
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\%{cfg.platform}\\fmodL_vc.lib $(OutDir)"
         }
     filter "configurations:Release"
-        links { "Vendor/openssl/lib/x64/Release/libcrypto.lib", "Vendor/openssl/lib/x64/Release/libssl.lib", "Vendor/fmod/lib/x64/fmod_vc.lib" }
+        links { "Vendor/openssl/lib/%{cfg.platform}/Release/libcrypto.lib", "Vendor/openssl/lib/%{cfg.platform}/Release/libssl.lib", "Vendor/fmod/lib/%{cfg.platform}/fmod_vc.lib" }
         postbuildcommands {
-            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Release\\ossl_static.pdb $(OutDir)",
-            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\x64\\fmod_vc.lib $(OutDir)"
+            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\%{cfg.platform}\\Release\\ossl_static.pdb $(OutDir)",
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\%{cfg.platform}\\fmod_vc.lib $(OutDir)"
         }
     files {
         "premake5.lua",
