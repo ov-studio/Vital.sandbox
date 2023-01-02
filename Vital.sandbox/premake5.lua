300a1d4f2cf3a9016913ccd017380b7e9480d7a8
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 15:03:16 +0300

    Update premake5.lua

--- Vital.sandbox/premake5.lua
+++ Vital.sandbox/premake5.lua
@@ -9,13 +9,13 @@ project "Vital.sandbox"
         links { "Vendor/openssl/lib/x64/Debug/libcrypto.lib", "Vendor/openssl/lib/x64/Debug/libssl.lib", "Vendor/fmod/lib/x64/fmodL_vc.lib" }
         postbuildcommands {
             "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Debug\\ossl_static.pdb $(OutDir)",
-            "copy /y ..\\Vital.sandbox\\Vendor\\fmod/lib\\x64\\fmodL_vc.lib $(OutDir)"
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\x64\\fmodL_vc.lib $(OutDir)"
         }
     filter "configurations:Release"
         links { "Vendor/openssl/lib/x64/Release/libcrypto.lib", "Vendor/openssl/lib/x64/Release/libssl.lib", "Vendor/fmod/lib/x64/fmod_vc.lib" }
         postbuildcommands {
             "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Release\\ossl_static.pdb $(OutDir)",
-            "copy /y ..\\Vital.sandbox\\Vendor\\fmod/lib\\x64\\fmod_vc.lib $(OutDir)"
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod\\lib\\x64\\fmod_vc.lib $(OutDir)"
         }
     files {
         "premake5.lua",
