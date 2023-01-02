26ddb02e5e375d84a16f177c209f53e5b55d7588
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 08:22:53 +0300

    Update premake5.lua

--- Vital.sandbox/premake5.lua
+++ Vital.sandbox/premake5.lua
@@ -1,15 +1,15 @@
 project "Vital.sandbox"
-	language "C++"
-	cppdialect "C++17"
-	kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    kind "StaticLib"
     targetdir ("../.build/"..outputdir.."/%{prj.name}")
     objdir ("../.output/"..outputdir.."/%{prj.name}")
     filter "configurations:Debug"
         includedirs { ".", "Vendor", "Vendor/openssl/x64/Debug/include/openssl", "Vendor/fmod/include", "Vendor/lua", "Vendor/duktape" }
     filter "configurations:Release"
         includedirs { ".", "Vendor", "Vendor/openssl/x64/Release/include/openssl", "Vendor/fmod/include", "Vendor/lua", "Vendor/duktape" }
-	files {
-		"premake5.lua",
-		"**.cpp",
-		"**.h"
-	}
+    files {
+        "premake5.lua",
+        "**.cpp",
+        "**.h"
+    }
