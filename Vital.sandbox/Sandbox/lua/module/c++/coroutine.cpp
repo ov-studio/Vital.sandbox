78a8f0ab7bd5c51ee9085800eec6caa2a5098452
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 17:42:35 +0300

    Update coroutine.cpp

--- Vital.sandbox/Sandbox/lua/module/c++/coroutine.cpp
+++ Vital.sandbox/Sandbox/lua/module/c++/coroutine.cpp
@@ -26,6 +26,49 @@ namespace Vital::Sandbox::Lua::API {
     void vSandbox_Coroutine() {
         if (isBound) return; isBound = true;
 
+        /*
+        ** Resumes a coroutine. Returns the number of results for non-error
+        ** cases or -1 for errors.
+        */
+        static int auxresume (lua_State *L, lua_State *co, int narg) {
+            int status, nres;
+            if (l_unlikely(!lua_checkstack(co, narg))) {
+                lua_pushliteral(L, "too many arguments to resume");
+                return -1;  /* error flag */
+            }
+            lua_xmove(L, co, narg);
+            status = lua_resume(co, L, narg, &nres);
+            if (l_likely(status == LUA_OK || status == LUA_YIELD)) {
+                if (l_unlikely(!lua_checkstack(L, nres + 1))) {
+                lua_pop(co, nres);  /* remove results anyway */
+                lua_pushliteral(L, "too many results to resume");
+                return -1;  /* error flag */
+                }
+                lua_xmove(co, L, nres);  /* move yielded values */
+                return nres;
+            }
+            else {
+                lua_xmove(co, L, 1);  /* move error message */
+                return -1;  /* error flag */
+            }
+        }
+
+        static int luaB_coresume (lua_State *L) {
+            lua_State *co = getco(L);
+            int r;
+            r = auxresume(L, co, lua_gettop(L) - 1);
+            if (l_unlikely(r < 0)) {
+                lua_pushboolean(L, 0);
+                lua_insert(L, -2);
+                return 2;  /* return false + error message */
+            }
+            else {
+                lua_pushboolean(L, 1);
+                lua_insert(L, -(r + 1));
+                return r + 1;  /* return true + 'resume' returns */
+            }
+        }
+
         bind("coroutine", "create", [](vital_ref* ref) -> int {
             auto vm = fetchVM(ref);
             return vm -> execute([&]() -> int {
@@ -51,8 +94,8 @@ namespace Vital::Sandbox::Lua::API {
         bind("coroutine", "pause", [](vital_ref* ref) -> int {
             auto vm = fetchVM(ref);
             return vm -> execute([&]() -> int {
-                if ((vm -> getArgCount() < 1) || (!vm -> isThread(1))) throw ErrorCode["invalid-arguments"];
-                lua_yield(ref, 0);
+                if (!isThread) throw throw ErrorCode["invalid-thread"];
+                vm -> pause();
                 return 1;
             });
         });
@@ -62,7 +105,9 @@ namespace Vital::Sandbox::Lua::API {
             return vm -> execute([&]() -> int {
                 if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw ErrorCode["invalid-arguments"];
                 auto duration = vm -> getInt(1);
+                // TODO: Pause here
                 auto thread = Vital::System::Thread::create([&]() -> void {
+                    // TODO: Resume here
                     std::this_thread::sleep_for(std::chrono::milliseconds(duration));
                 });
                 return 1;
