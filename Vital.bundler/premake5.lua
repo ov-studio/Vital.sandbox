350e147320b8fb3ff245c5829309067c67acaec7
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 08:22:57 +0300

    Update premake5.lua

--- Vital.bundler/premake5.lua
+++ Vital.bundler/premake5.lua
@@ -1,12 +1,13 @@
 project "Vital.bundler"
-	language "C++"
-	cppdialect "C++17"
-	kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    kind "ConsoleApp"
     targetdir ("../.build/"..outputdir.."/%{prj.name}")
     objdir ("../.output/"..outputdir.."/%{prj.name}")
-	includedirs { "." }
-	files {
-		"premake5.lua",
-		"**.cpp",
-		"**.h"
-	}
+    includedirs { ".", "../Vital.sandbox" }
+    links { "Vital.sandbox" }
+    files {
+        "premake5.lua",
+        "**.cpp",
+        "**.h"
+    }
