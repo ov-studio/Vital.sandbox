9b6fdf61cc718eb7aaec068509bc2160db07c567
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-01-02 15:01:15 +0300

    Update premake5.lua

--- Vital.sandbox/premake5.lua
+++ Vital.sandbox/premake5.lua
@@ -8,14 +8,14 @@ project "Vital.sandbox"
     filter "configurations:Debug"
         links { "Vendor/openssl/lib/x64/Debug/libcrypto.lib", "Vendor/openssl/lib/x64/Debug/libssl.lib", "Vendor/fmod/lib/x64/fmodL_vc.lib" }
         postbuildcommands {
-            "copy /y Vendor/openssl/lib/x64/Debug/ossl_static.pdb %{cfg.targetdir}",
-            "copy /y Vendor/fmod/lib/x64/fmodL_vc.lib %{cfg.targetdir}"
+            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Debug\\ossl_static.pdb $(OutDir)",
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod/lib\\x64\\fmodL_vc.lib $(OutDir)"
         }
     filter "configurations:Release"
         links { "Vendor/openssl/lib/x64/Release/libcrypto.lib", "Vendor/openssl/lib/x64/Release/libssl.lib", "Vendor/fmod/lib/x64/fmod_vc.lib" }
         postbuildcommands {
-            "copy /y ../Vital.sandbox/Vendor/openssl/lib/x64/Release/ossl_static.pdb %{cfg.targetdir}",
-            "copy /y ../Vital.sandbox/Vendor/fmod/lib/x64/fmod_vc.lib %{cfg.targetdir}"
+            "copy /y ..\\Vital.sandbox\\Vendor\\openssl\\lib\\x64\\Release\\ossl_static.pdb $(OutDir)",
+            "copy /y ..\\Vital.sandbox\\Vendor\\fmod/lib\\x64\\fmod_vc.lib $(OutDir)"
         }
     files {
         "premake5.lua",
