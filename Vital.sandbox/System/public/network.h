23e1f33d16ea47f13eb888efc457ac751d8ad92f
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-02-21 19:47:48 +0300

    Init network system

---
+++ Vital.sandbox/System/public/network.h
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: System: public: network.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Network System
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/vital.h>
+
+
+///////////////////////////
+// Vital: Type: Network //
+///////////////////////////
+
+namespace Vital::System::Network {
+    extern bool isConnected();
+    extern bool create(Vital::Type::Network::Address address);
+    extern bool destroy();
+    extern bool render();
+    extern bool setPeerLimit(int limit);
+    extern int getPeerLimit();
+    extern bool setBandwidthLimit(Vital::Type::Network::Bandwidth limit);
+    extern bool emit(const std::string& buffer, Vital::Type::Network::PeerID peer = 0);
+    extern Vital::Type::Network::Bandwidth getBandwidthLimit();
+}
