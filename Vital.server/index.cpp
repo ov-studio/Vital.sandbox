5bb2a8034d9a9da4890648ef2f58c0b89a3cfaf4
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2023-02-22 21:32:45 +0300

    Init Vital.server

---
+++ Vital.server/index.cpp
@@ -0,0 +1,68 @@
+﻿/*----------------------------------------------------------------
+     Resource: Vital.server
+     Script: index.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Bundler Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/audio.h>
+#include <Sandbox/lua/public/api.h>
+#include <Sandbox/js/public/api.h>
+
+
+///////////
+// Root //
+//////////
+
+
+
+int main() {
+    Vital::System::setPlatform("server");
+    Vital::Sandbox::Lua::API::boot();
+    Vital::Sandbox::Lua::API::onErrorHandle([](const std::string& err) -> void {
+        std::cout << "\n" << err;
+    });
+
+    std::string rwString = R"(
+        print("\n")
+        local buffer = "test"
+        --print(crypto.decode(buffer))
+
+        timer:create(function()
+            print("EXECUTED TIMER")
+        end, 5000, 3)
+        print("Tail stack reached")
+    )";
+    //std::cout<<"\n Main Thread : " << std::this_thread::get_id();
+    auto testVM = new Vital::Sandbox::Lua::create();
+    testVM -> loadString(rwString);
+
+    /*
+    Vital::Sandbox::JS::API::boot();
+    Vital::Sandbox::JS::API::onErrorHandle([](const std::string& err) -> void {
+        std::cout << "\n" << err;
+    });
+    std::string rwString = R"(
+    test = function() {
+        return Duktape;
+    }
+    test()
+    )";
+    auto testVM = new Vital::Sandbox::JS::create();
+    testVM -> loadString(rwString);
+    std::string result = testVM -> getString(-1);
+    std::cout << "\n" << "RESULT: " << result;
+    */
+
+    do {
+    } while (true);
+    return 1;
+}
---
+++ Vital.server/premake5.lua
@@ -0,0 +1,15 @@
+project "Vital.server"
+    language "C++"
+    cppdialect "C++17"
+    kind "ConsoleApp"
+    targetdir ("../.build/"..outputdir.."/%{prj.name}")
+    objdir ("../.output/"..outputdir.."/%{prj.name}")
+    includedirs { ".", "../Vital.sandbox" }
+    links { "Vital.sandbox" }
+    files {
+        "premake5.lua",
+        "**.cpp",
+        "**.c",
+        "**.hpp",
+        "**.h"
+    }
