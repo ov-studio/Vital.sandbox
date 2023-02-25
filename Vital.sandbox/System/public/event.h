a648b7034e5825be3ac7b1d99bc49b224f74005a
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-02-26 00:15:40 +0300

    Update event.h

--- Vital.sandbox/System/public/event.h
+++ Vital.sandbox/System/public/event.h
@@ -21,7 +21,7 @@
 /////////////////////////
 
 namespace Vital::System::Event {
-    extern bool bind(const std::string& identifier, Vital::Type::Event::Handler exec);
-    extern bool unbind(const std::string& identifier, Vital::Type::Event::Handler exec);
+    extern bool bind(const std::string& identifier, Vital::Type::Event::Handler& exec);
+    extern bool unbind(const std::string& identifier, Vital::Type::Event::Handler& exec);
     extern bool emit(const std::string& identifier, Vital::Type::Event::Arguments arguments = {});
 }
