5806cd10898f5f7e33cdeb77e7e3d8aca5bf2ac4
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-02-25 21:00:55 +0300

    Created argument class to better organize

--- Vital.sandbox/Type/public/event.h
+++ Vital.sandbox/Type/public/event.h
@@ -21,44 +21,47 @@
 /////////////////////////
 
 namespace Vital::Type::Event {
-    typedef std::map<std::string, std::pair<std::string, std::string>> Arguments;
+    class Argument {
+        private:
+            std::string buffer, type;
+        public:
+            inline Argument(const std::string& arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(int arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(long arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(long long arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(unsigned arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(unsigned long arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(unsigned long long arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(float arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(double arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+            inline Argument(long double arg) { buffer = arg, type = std::string(typeid(arg).name()); }
+
+            template<typename T>
+            T value() {
+                switch(type) {
+                    typeid(const std::string&).name():
+                        return buffer;
+                    typeid(int).name():
+                        return reinterpret_cast<int>(buffer);
+                    typeid(long).name():
+                        return reinterpret_cast<long>(buffer);
+                    typeid(long long).name():
+                        return reinterpret_cast<long long>(buffer);
+                    typeid(unsigned).name():
+                        return reinterpret_cast<unsigned>(buffer);
+                    typeid(unsigned long).name():
+                        return reinterpret_cast<unsigned long>(buffer);
+                    typeid(unsigned long long).name():
+                        return reinterpret_cast<unsigned long long>(buffer);
+                    typeid(float).name():
+                        return reinterpret_cast<float>(buffer);
+                    typeid(double).name():
+                        return reinterpret_cast<double>(buffer);
+                    typeid(long double).name():
+                        return reinterpret_cast<long double>(buffer);
+                }
+            }
+    };
+    typedef std::map<std::string, Argument> Arguments;
     typedef std::function<void(Arguments)> Handler;
-
-    inline std::pair<std::string, std::string> Argument(const std::string& arg) { return {arg, std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(int arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(long arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(long long arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(unsigned arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(unsigned long arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(unsigned long long arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(float arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(double arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-    inline std::pair<std::string, std::string> Argument(long double arg) { return {std::to_string(arg), std::string(typeid(arg).name())}; }
-
-    template<typename T>
-    T ArgumentValue(std::pair<std::string, std::string> arg) {
-        switch(arg.second) {
-            typeid(const std::string&).name():
-                return arg.first;
-            typeid(int).name():
-                return reinterpret_cast<int>(arg.first);
-            typeid(long).name():
-                return reinterpret_cast<long>(arg.first);
-            typeid(long long).name():
-                return reinterpret_cast<long long>(arg.first);
-            typeid(unsigned).name():
-                return reinterpret_cast<unsigned>(arg.first);
-            typeid(unsigned long).name():
-                return reinterpret_cast<unsigned long>(arg.first);
-            typeid(unsigned long long).name():
-                return reinterpret_cast<unsigned long long>(arg.first);
-            typeid(float).name():
-                return reinterpret_cast<float>(arg.first);
-            typeid(double).name():
-                return reinterpret_cast<double>(arg.first);
-            typeid(long double).name():
-                return reinterpret_cast<long double>(arg.first);
-        }
-        throw ErrorCode["invalid-arguments"];
-    }
 }
