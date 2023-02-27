59127a91c59705c91239e95b9485fb175197aa42
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-02-27 20:36:53 +0300

    Update timer.cpp

--- Vital.sandbox/Type/private/timer.cpp
+++ Vital.sandbox/Type/private/timer.cpp
@@ -24,14 +24,15 @@ namespace Vital::Type::Timer {
     // Instantiators //
     Instance::Instance(std::function<void(Vital::Type::Timer::Instance*)> exec, int interval, int executions) {
         targetInterval = interval, targetExecutions = executions;
-        Vital::Type::Thread::Instance([=](Vital::Type::Thread::Instance* thread) -> void {
+        Vital::Type::Thread::Instance([&](Vital::Type::Thread::Instance* thread) -> void {
             while (!isUnloaded && ((targetExecutions == 0) || (currentExecutions < targetExecutions))) {
                 thread -> sleep(interval);
                 currentExecutions++;
                 exec(this);
             }
-            delete this;
+            isUnloaded = true;
         });
+        while (!isUnloaded) { isUnloaded = isUnloaded; }
     }
     Instance::~Instance() {
         isUnloaded = true;
