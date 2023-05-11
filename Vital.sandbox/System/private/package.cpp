d1d044ed4588bae7e874fed0936c67592fe446cb
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-05-12 02:44:46 +0300

    Added package system

---
+++ Vital.sandbox/System/private/package.cpp
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: System: private: package.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Package System
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/package.h>
+#include <System/public/crypto.h>
+#include <System/public/file.h>
+
+
+/////////////////////////////
+// Vital: System: Package //
+/////////////////////////////
+
+namespace Vital::System::Package {
+    bool create(std::string& path, std::vector<std::pair<std::string, char>> modules, bool isDebugMode) {
+        Vital::System::File::resolve(path);
+        std::string result = "std::string fetchPackage(const std::string& path) {";
+        result += "\nstd::string result;";
+        if (isDebugMode) std::cout << "\nPackaging: " << path << "...";
+        for (const auto& i : modules) {
+            auto module = i.first;
+            if (!Vital::System::File::exists(module)) {
+                if (isDebugMode) std::cout << "\nInvalid Module: " << module;
+                return false;
+            }
+            else {
+                result += "\nif (path == \"" + i.first + "\") {\n";
+                result += Vital::System::Package::Module::write(Vital::System::File::read(module), i.second);
+                result += "\n}";
+                if (isDebugMode) std::cout << "\nBundled Module: " << module;
+            }
+        }
+        result += "\nreturn Vital::System::Package::Module::read(buffer);";
+        result += "\n}";
+        Vital::System::File::write(path, result);
+        if (isDebugMode) std::cout << "\nPackaged: " << path;
+        return true
+    }
+}
+
+
+/////////////////////////////////////
+// Vital: System: Package: Module //
+/////////////////////////////////////
+
+namespace Vital::System::Package::Module {
+    std::string read(std::vector<std::string> buffer) {
+        std::string result;
+        for (const auto& i : buffer) {
+            result += Vital::System::Crypto::decode(i);
+        }
+        return result;
+    }
+
+    std::string write(const std::string& buffer, const char delimiter) {
+        std::string result = "std::vector<std::string> buffer;";
+        std::string chunk;
+        std::stringstream stream(buffer);
+        while (std::getline(stream, chunk, delimiter)) {
+            result += "\nbuffer.push_back(\"" + Vital::System::Crypto::encode(chunk + delimiter) + "\");";
+        }
+        return result;
+    }
+}
