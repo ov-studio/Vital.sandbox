d5c26e285d26c5503bdf09fdad3e6bd2ae65254c
Tron <103286142+ov-anisa@users.noreply.github.com>
2025-02-02 02:40:17 +0300

    Update string.lua

--- Vital.sandbox/Sandbox/lua/module/string.lua
+++ Vital.sandbox/Sandbox/lua/module/string.lua
@@ -25,76 +25,122 @@ local imports = {
 --[[ Class: String ]]--
 -----------------------
 
-local string = class:create("string", utf8)
-for i, j in imports.pairs(imports.string) do
-    string.public[i] = (not string.public[i] and j) or string.public[i]
-end
+local module = {
+    ["stringn"] = class:create("stringn", string),
+    ["string"] = class:create("string", table.clone(utf8))
+}
 utf8 = nil
-string.private.minifier = crypto.hash("SHA256", "vStudio")
-
-function string.public.isVoid(baseString)
-    if not baseString or (imports.type(baseString) ~= "string") then return false end
-    baseString = string.public.gsub(baseString, "[\n\r\t%s]", "")
-    return (not string.public.match(baseString, "[%W%w]") and true) or false
-end
-
-local __string_len = string.public.len
-function string.public.len(baseString)
-    if not baseString or (imports.type(baseString) ~= "string") then return false end
-    return __string_len(baseString)
-end
-
-local __string_gsub = string.public.gsub
-function string.public.gsub(baseString, matchWord, replaceWord, matchLimit, isStrictcMatch, matchPrefix, matchPostfix)
-    if not baseString or (imports.type(baseString) ~= "string") or not matchWord or (imports.type(matchWord) ~= "string") or not replaceWord or (imports.type(replaceWord) ~= "string") then return false end
-    matchPrefix, matchPostfix = (matchPrefix and (imports.type(matchPrefix) == "string") and matchPrefix) or "", (matchPostfix and (imports.type(matchPostfix) == "string") and matchPostfix) or ""
-    matchWord = (isStrictcMatch and "%f[^"..matchPrefix.."%z%s]"..matchWord.."%f["..matchPostfix.."%z%s]") or matchPrefix..matchWord..matchPostfix
-    return __string_gsub(baseString, matchWord, replaceWord, matchLimit)
-end
-
-function string.public.parse(baseString)
-    if not baseString then return false end
-    if imports.tostring(baseString) == "nil" then return
-    elseif imports.tostring(baseString) == "false" then return false
-    elseif imports.tostring(baseString) == "true" then return true
-    else return imports.tonumber(baseString) or baseString end
-end
 
 function string.public.parseHex(baseString)
-    if not baseString then return false end
-    baseString = string.public.gsub(baseString, "#", "")
-    return imports.tonumber("0x"..string.public.sub(baseString, 1, 2)) or 0, imports.tonumber("0x"..string.public.sub(baseString, 3, 4)) or 0, imports.tonumber("0x"..string.public.sub(baseString, 5, 6)) or 0
+for i, j in imports.pairs(imports.string) do
+    module.string.public[i] = (not module.string.public[i] and j) or module.string.public[i]
 end
 
-function string.public.split(baseString, separator)
-    if not baseString or (imports.type(baseString) ~= "string") or not separator or (imports.type(separator) ~= "string") then return false end
-    baseString = baseString..string.public.match(separator, separator)
-    local result = {}
-    for matchValue in string.public.gmatch(baseString, "(.-)"..separator) do
-        table.insert(result, matchValue)
+for i, j in pairs(module) do
+    function j.public.isVoid(baseString)
+        if not baseString or (imports.type(baseString) ~= "string") then return false end
+        return (not j.public.find(baseString, "[%S]") and true) or false
     end
-    return result
-end
-
-function string.public.kern(baseString, kerner)
-    if not baseString or (imports.type(baseString) ~= "string") then return false end
-    return string.public.sub(string.public.gsub(baseString, ".", (kerner or " ").."%0"), 2)
-end
-
-function string.public.detab(baseString)
-    if not baseString or (imports.type(baseString) ~= "string") then return false end
-    return string.public.gsub(baseString, "\t", "    ")
-end
-
-function string.public.minify(baseString)
-    if not baseString or (imports.type(baseString) ~= "string") then return false end
-    local result = ""
-    for i = 1, string.public.len(baseString), 1 do
-        result = result..(string.private.minifier)..string.public.byte(baseString, i)
+    
+    local raw_len = j.public.len
+    function j.public.len(baseString)
+        if not baseString or (imports.type(baseString) ~= "string") then return false end
+        return raw_len(baseString)
     end
-    return [[
-    local b, __b = string.split("]]..result..[[", "]]..(string.private.minifier)..[["), ""
-    for i = 1, table.length(b), 1 do __b = __b..(string.char(b[i]) or "") end
-    engine.loadString(__b)()
+    
+    function j.public.parse(baseString)
+        if not baseString then return false end
+        if imports.tostring(baseString) == "nil" then return
+        elseif imports.tostring(baseString) == "false" then return false
+        elseif imports.tostring(baseString) == "true" then return true
+        else return imports.tonumber(baseString) or baseString end
+    end
+    
+    function j.public.parseHex(baseString)
+        if not baseString then return false end
+        baseString = j.public.gsub(baseString, "#", "")
+        return imports.tonumber("0x"..j.public.sub(baseString, 1, 2)) or 0, imports.tonumber("0x"..j.public.sub(baseString, 3, 4)) or 0, imports.tonumber("0x"..j.public.sub(baseString, 5, 6)) or 0
+    end
+    
+    function j.public.formatTime(milliseconds)
+        milliseconds = imports.tonumber(milliseconds)
+        if not milliseconds then return false end
+        milliseconds = math.floor(milliseconds)
+        local totalSeconds = math.floor(milliseconds/1000)
+        local seconds = totalSeconds%60
+        local minutes = math.floor(totalSeconds/60)
+        local hours = math.floor(minutes/60)
+        minutes = minutes%60
+        return imports.j.format("%02d:%02d:%02d", hours, minutes, seconds)
+    end
+    
+    function j.public.split(baseString, separator)
+        if not baseString or (imports.type(baseString) ~= "string") or not separator or (imports.type(separator) ~= "string") then return false end
+        local result = {}
+        local index = 1
+        local length = j.public.len(separator)
+        while(true) do
+            local ref = j.public.find(baseString, separator, index, true)
+            if not ref then
+                table.insert(result, j.public.sub(baseString, index))
+                break
+            end
+            table.insert(result, j.public.sub(baseString, index, ref - 1))
+            index = ref + length
+        end 
+    end
+end
+
+    function j.public.kern(baseString, kerner)
+        if not baseString or (imports.type(baseString) ~= "string") then return false end
+        return j.public.sub(j.public.gsub(baseString, ".", (kerner or " ").."%0"), 2)
+    end
+    
+    function j.public.detab(baseString)
+        if not baseString or (imports.type(baseString) ~= "string") then return false end
+        return j.public.gsub(baseString, "\t", "    ")
+    end
+    
+    function j.public.minify(baseString)
+        if not baseString or (imports.type(baseString) ~= "string") then return false end
+        baseString = j.public.gsub(baseString, "%-%-%[%[(.-)%]%]", "") --Removes single-line comments
+        baseString = j.public.gsub(baseString, "%-%-.-\n", "") --Removes multi-line comments
+        do
+            --Encodes strings into bytes
+            local index, key, result = 1, false, [[
+                local function vsdk_processbyte(byte)
+                    local result = ""
+                    for num in ]]..i..[[.gmatch(byte, "(%d+):") do
+                        result = result..]]..i..[[.char(num)
+                    end
+                    return result
+                end
+            ]]
+            repeat
+                local rw = j.public.sub(baseString, index, index)
+                local isEscaped = j.public.sub(baseString, index - 1, index - 1) == "\\"
+                if not key then
+                    if not isEscaped and ((rw == "\"") or (rw == "\'")) then
+                        key = rw
+                        result = result.."vsdk_processbyte(\""
+                    else
+                        result = result..rw
+                    end
+                else
+                    if not isEscaped and (key == rw) then
+                        key = false
+                        result = result.."\")"
+                    else
+                        result = result..j.public.byte(rw)..":"
+                    end
+                end
+                index = index + 1
+            until(index > #baseString)
+            baseString = result
+        end
+        baseString = j.public.gsub(baseString, "\n", " ") --Removes newlines
+        baseString = j.public.gsub(baseString, "%s+$", "") --Removes trailing spaces
+        baseString = j.public.gsub(baseString, "%s+", " ") --Removes trailing spaces
+    end
     ]]
 end
