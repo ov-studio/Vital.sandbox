24429fe0b0db9ac0ae69b64a4cc162de7e7fc80a
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-02-09 01:30:09 +0300

    Revert "Removed bind map from header files"
    This reverts commit 8e4c7ec9b5cd8603d2340e5cbbba825ff5f6dec2.

--- Vital.sandbox/Sandbox/js/public/api.h
+++ Vital.sandbox/Sandbox/js/public/api.h
@@ -22,6 +22,7 @@
 
 namespace Vital::Sandbox::JS::API {
     // Handlers //
+    extern std::map<vital_bind, vital_exec> vmBind;
     extern bool onErrorHandle(std::function<void(const std::string&)> exec);
 
     // Helpers //
