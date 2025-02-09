619c29e0232952fb0bdf7a5de88e5dfd54a18da6
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-02-09 08:17:24 +0300

    moved template based getters into header file

--- Vital.sandbox/Type/private/stack.cpp
+++ Vital.sandbox/Type/private/stack.cpp
@@ -22,51 +22,33 @@
 
 namespace Vital::Type {
     // Checkers //
-    bool Stack::isNil(int index) { return ((index < 0) || rwVector.empty() || (index >= rwVector.size()) && true) || false; };
-    bool Stack::isNil(const std::string& index) { return rwMap.find(index) == rwMap.end(); };
-    bool Stack::isString(int index) { return (!isNil(index) && rwVector.at(index).isString() && true) || false; }
-    bool Stack::isString(const std::string& index) { return (!isNil(index) && rwMap.at(index).isString() && true) || false; }
-    bool Stack::isNumber(int index) { return (!isNil(index) && rwVector.at(index).isNumber() && true) || false; }
-    bool Stack::isNumber(const std::string& index) { return (!isNil(index) && rwMap.at(index).isNumber() && true) || false; }
-
-    // Getters //
-    template <typename T>
-    T Stack::get(int index) {
-        if (isNil(index)) throw ErrorCode["invalid-result"];
-        return rwVector.at(index).get<T>();
-    }
-    template <typename T>
-    T Stack::get(const std::string& index) {
-        if (isNil(index)) throw ErrorCode["invalid-result"];
-        return rwMap.at(index).get<T>();
-    }
-
-    // Pushers //
-    template <typename T>
-    void Stack::push(T& value) { rwVector.push_back(Value(value)); };
-    template <typename T>
-    void Stack::push(const std::string& index, T& value) { rwMap.emplace(index, Value(value)); };
+    bool Stack::isNil(int index) { return ((index < 0) || vector.empty() || (index >= vector.size()) && true) || false; };
+    bool Stack::isNil(const std::string& index) { return map.find(index) == map.end(); };
+    bool Stack::isString(int index) { return (!isNil(index) && vector.at(index).isString() && true) || false; }
+    bool Stack::isString(const std::string& index) { return (!isNil(index) && map.at(index).isString() && true) || false; }
+    bool Stack::isNumber(int index) { return (!isNil(index) && vector.at(index).isNumber() && true) || false; }
+    bool Stack::isNumber(const std::string& index) { return (!isNil(index) && map.at(index).isNumber() && true) || false; }
 
     // Poppers //
-    void Stack::pop(int index) { rwVector.erase(rwVector.begin() + index - 1); }
-    void Stack::pop(const std::string& index) { rwMap.erase(index); }
+    void Stack::pop(int index) { vector.erase(vector.begin() + index - 1); }
+    void Stack::pop(const std::string& index) { map.erase(index); }
 
     // Utils //
     std::string Stack::serialize() {
         std::ostringstream stream;
         const auto typeSize = sizeof(size_t);
-        const size_t rwVectorSize = rwVector.size(), rwMapSize = rwMap.size();
+        const size_t rwVectorSize = vector.size(), rwMapSize = map.size();
         stream.write(reinterpret_cast<const char*>(&rwVectorSize), typeSize);
         stream.write(reinterpret_cast<const char*>(&rwMapSize), typeSize);
         for (int i = 0; i < rwVectorSize; i++) {
-            auto value = rwVector.at(i).serialize();
+            auto value = vector.at(i).serialize();
             const size_t valueTypeSize = value.first.size(), valueSize = value.second.size();
             stream.write(reinterpret_cast<const char*>(&valueTypeSize), typeSize);
             stream.write(reinterpret_cast<const char*>(&valueSize), typeSize);
             stream.write(value.first.c_str(), valueTypeSize);
             stream.write(value.second.c_str(), valueSize);
         }
-        for (auto& i : rwMap) {
+        for (auto& i : map) {
             auto value = i.second.serialize();
             const size_t indexSize = i.first.size(), valueTypeSize = value.first.size(), valueSize = value.second.size();
             stream.write(reinterpret_cast<const char*>(&indexSize), typeSize);
@@ -113,97 +95,5 @@ namespace Vital::Type {
             delete[] value;
         }
         return stack;
-    }
-}
-
-namespace Vital::Type {
-    // Checkers //
-    bool Stack::Value::isString() { return std::holds_alternative<std::string>(value); }
-    bool Stack::Value::isNumber() {
-        return (
-            std::holds_alternative<int>(value) || 
-            std::holds_alternative<float>(value) || 
-            std::holds_alternative<double>(value) || 
-            std::holds_alternative<long>(value) || 
-            std::holds_alternative<long long>(value) || 
-            std::holds_alternative<long double>(value) || 
-            std::holds_alternative<unsigned>(value) || 
-            std::holds_alternative<unsigned long>(value) || 
-            std::holds_alternative<unsigned long long>(value)
-        );
-    }
-
-    // Utils
-    std::pair<std::string, std::string> Stack::Value::serialize() {
-        if (isString()) return {"std::string", get<std::string>()};
-        if (isNumber()) {
-            std::ostringstream oss;
-            std::string type;
-            if (std::holds_alternative<int>(value)) {
-                type = "int";
-                oss << get<int>();
-            } else if (std::holds_alternative<float>(value)) {
-                type = "float";
-                oss << get<float>();
-            } else if (std::holds_alternative<double>(value)) {
-                type = "double";
-                oss << get<double>();
-            } else if (std::holds_alternative<long>(value)) {
-                type = "long";
-                oss << get<long>();
-            } else if (std::holds_alternative<long long>(value)) {
-                type = "long long";
-                oss << get<long long>();
-            } else if (std::holds_alternative<long double>(value)) {
-                type = "long double";
-                oss << get<long double>();
-            } else if (std::holds_alternative<unsigned int>(value)) {
-                type = "unsigned int";
-                oss << get<unsigned int>();
-            } else if (std::holds_alternative<unsigned long>(value)) {
-                type = "unsigned long";
-                oss << get<unsigned long>();
-            } else if (std::holds_alternative<unsigned long long>(value)) {
-                type = "unsigned long long";
-                oss << get<unsigned long long>();
-            } 
-            return {type, oss.str()};
-        }
-        throw std::runtime_error("invalid-result");
-    }
-    Stack::Value Stack::Value::deserialize(const std::pair<std::string, std::string>& serial) {
-        const auto& rwType = serial.first;
-        const auto& rwValue = serial.second;
-        if (rwType == "std::string") {
-            return Value(rwValue);
-        } 
-        if (rwType == "int") {
-            return Value(std::stoi(rwValue));
-        } 
-        if (rwType == "float") {
-            return Value(std::stof(rwValue));
-        } 
-        if (rwType == "double") {
-            return Value(std::stod(rwValue));
-        } 
-        if (rwType == "long") {
-            return Value(std::stol(rwValue));
-        } 
-        if (rwType == "long long") {
-            return Value(std::stoll(rwValue));
-        } 
-        if (rwType == "long double") {
-            return Value(std::stold(rwValue));
-        } 
-        if (rwType == "unsigned int") {
-            return Value(static_cast<unsigned int>(std::stoul(rwValue)));
-        } 
-        if (rwType == "unsigned long") {
-            return Value(std::stoul(rwValue));
-        } 
-        if (rwType == "unsigned long long") {
-            return Value(std::stoull(rwValue));
-        } 
-        throw std::runtime_error("invalid-result");
     }
 }
--- Vital.sandbox/Type/public/stack.h
+++ Vital.sandbox/Type/public/stack.h
@@ -25,8 +25,8 @@ namespace Vital::Type {
         public:
             class Value;
         private:
-            std::vector<Value> rwVector;
-            std::map<std::string, Value> rwMap;
+            std::vector<Value> vector;
+            std::map<std::string, Value> map;
         public:
             class Value {
                 private:
@@ -37,16 +37,98 @@ namespace Vital::Type {
                     Value(T argument) : value(argument) {}
 
                     // Checkers //
-                    bool isString();
-                    bool isNumber();
+                    bool isString() { return std::holds_alternative<std::string>(value); }
+                    bool isNumber() {
+                        return (
+                            std::holds_alternative<int>(value) || 
+                            std::holds_alternative<float>(value) || 
+                            std::holds_alternative<double>(value) || 
+                            std::holds_alternative<long>(value) || 
+                            std::holds_alternative<long long>(value) || 
+                            std::holds_alternative<long double>(value) || 
+                            std::holds_alternative<unsigned>(value) || 
+                            std::holds_alternative<unsigned long>(value) || 
+                            std::holds_alternative<unsigned long long>(value)
+                        );
+                    }
 
                     // Getters //
                     template <typename T>
                     T get() const { return std::get<T>(value); }
 
                     // Utils
-                    std::pair<std::string, std::string> serialize();
-                    static Value deserialize(const std::pair<std::string, std::string>& serial);
+                    std::pair<std::string, std::string> serialize() {
+                        if (isString()) return {"std::string", get<std::string>()};
+                        if (isNumber()) {
+                            std::ostringstream oss;
+                            std::string type;
+                            if (std::holds_alternative<int>(value)) {
+                                type = "int";
+                                oss << get<int>();
+                            } else if (std::holds_alternative<float>(value)) {
+                                type = "float";
+                                oss << get<float>();
+                            } else if (std::holds_alternative<double>(value)) {
+                                type = "double";
+                                oss << get<double>();
+                            } else if (std::holds_alternative<long>(value)) {
+                                type = "long";
+                                oss << get<long>();
+                            } else if (std::holds_alternative<long long>(value)) {
+                                type = "long long";
+                                oss << get<long long>();
+                            } else if (std::holds_alternative<long double>(value)) {
+                                type = "long double";
+                                oss << get<long double>();
+                            } else if (std::holds_alternative<unsigned int>(value)) {
+                                type = "unsigned int";
+                                oss << get<unsigned int>();
+                            } else if (std::holds_alternative<unsigned long>(value)) {
+                                type = "unsigned long";
+                                oss << get<unsigned long>();
+                            } else if (std::holds_alternative<unsigned long long>(value)) {
+                                type = "unsigned long long";
+                                oss << get<unsigned long long>();
+                            } 
+                            return {type, oss.str()};
+                        }
+                        throw std::runtime_error("invalid-result");
+                    }
+                    static Value deserialize(const std::pair<std::string, std::string>& serial) {
+                        const auto& rwType = serial.first;
+                        const auto& rwValue = serial.second;
+                        if (rwType == "std::string") {
+                            return Value(rwValue);
+                        } 
+                        if (rwType == "int") {
+                            return Value(std::stoi(rwValue));
+                        } 
+                        if (rwType == "float") {
+                            return Value(std::stof(rwValue));
+                        } 
+                        if (rwType == "double") {
+                            return Value(std::stod(rwValue));
+                        } 
+                        if (rwType == "long") {
+                            return Value(std::stol(rwValue));
+                        } 
+                        if (rwType == "long long") {
+                            return Value(std::stoll(rwValue));
+                        } 
+                        if (rwType == "long double") {
+                            return Value(std::stold(rwValue));
+                        } 
+                        if (rwType == "unsigned int") {
+                            return Value(static_cast<unsigned int>(std::stoul(rwValue)));
+                        } 
+                        if (rwType == "unsigned long") {
+                            return Value(std::stoul(rwValue));
+                        } 
+                        if (rwType == "unsigned long long") {
+                            return Value(std::stoull(rwValue));
+                        } 
+                        throw std::runtime_error("invalid-result");
+                    }
             };
 
             // Checkers //
@@ -59,15 +141,21 @@ namespace Vital::Type {
 
             // Getters //
             template <typename T>
-            T get(int index = 0);
+            T get(int index = 0) {
+                if (isNil(index)) throw ErrorCode["invalid-result"];
+                return vector.at(index).get<T>();
+            }
             template <typename T>
-            T get(const std::string& index);
+            T get(const std::string& index) {
+                if (isNil(index)) throw ErrorCode["invalid-result"];
+                return map.at(index).get<int>();
+            }
 
             // Pushers //
             template <typename T>
-            void push(T& value);
+            void push(T value) { vector.push_back(Value(value)); }
             template <typename T>
-            void push(const std::string& index, T& value);
+            void push(const std::string& index, T value) { map.emplace(index, Value(value)); }
 
             // Poppers
             void pop(int index = 0);
