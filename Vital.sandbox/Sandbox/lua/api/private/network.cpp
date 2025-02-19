c94ea8a20a8a95e5b6800301882790c773839069
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-02-19 08:18:45 +0300

    set `network.execNetwork` `nil` instead of false

--- Vital.sandbox/Sandbox/lua/api/private/network.cpp
+++ Vital.sandbox/Sandbox/lua/api/private/network.cpp
@@ -51,8 +51,8 @@ namespace Vital::Sandbox::Lua::API {
         vm -> getGlobal("network");
         vm -> getTableField("execNetwork", -1);
         vm -> setReference(Vital::System::Crypto::hash("SHA256", "network.execNetwork"), -1);
-        vm -> pop(1);
-        vm -> registerBool("execNetwork", false); // Add register Nil
+        vm -> pop(2);
+        vm -> registerNil("execNetwork", "network");
     }
     
     void Network::execute(const std::string& name, const std::string& payload) {
