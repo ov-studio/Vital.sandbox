63dc186d73cf9c6717d136007711ab2cb174ac28
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-02-19 10:23:06 +0300

    Update network.lua

--- Vital.sandbox/Sandbox/lua/module/network.lua
+++ Vital.sandbox/Sandbox/lua/module/network.lua
@@ -39,9 +39,9 @@ function network.private.fetchArg(index, pool)
     index = imports.tonumber(index) or 1
     index = (((index - math.floor(index)) == 0) and index) or 1
     if not pool or (imports.type(pool) ~= "table") then return false end
-    local argValue = pool[index]
+    local value = pool[index]
     table.remove(pool, index)
-    return argValue
+    return value
 end
 
 function network.private.execNetwork(cNetwork, exec, payload)
