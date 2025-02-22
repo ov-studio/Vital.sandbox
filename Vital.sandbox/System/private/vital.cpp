2ebb5d8a9ec6956ddb65792c8f757fcb98e3801b
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-02-22 07:09:27 +0300

    Update vital.cpp

--- Vital.sandbox/System/private/vital.cpp
+++ Vital.sandbox/System/private/vital.cpp
@@ -54,22 +54,19 @@ namespace Vital::System {
             auto memory = Vital::System::Inspect::memory();
             auto network = Vital::System::Inspect::network();
             auto disk = Vital::System::Inspect::disk();
-            vsdk_serial = "[";
-            vsdk_serial += "\n\t\"System: " + Vital::System::Crypto::hash("SHA256", std::string(system.hardware_id.begin(), system.hardware_id.end())) + "\",";
-            vsdk_serial += "\n\t\"OS: " + Vital::System::Crypto::hash("SHA256", std::string(system.os_serial.begin(), system.os_serial.end())) + "\",";
-            vsdk_serial += "\n\t\"SMBIOS: " + Vital::System::Crypto::hash("SHA256", std::string(smbios.serial.begin(), smbios.serial.end())) + "\",";
-            vsdk_serial += "\n\t\"CPU: " + Vital::System::Crypto::hash("SHA256", std::string(cpu.id.begin(), cpu.id.end())) + "\",";
+            vsdk_serial += "\"System\": \"" + Vital::System::Crypto::hash("SHA256", std::string(system.hardware_id.begin(), system.hardware_id.end())) + "\"";
+            vsdk_serial += "\n\"OS\": \"" + Vital::System::Crypto::hash("SHA256", std::string(system.os_serial.begin(), system.os_serial.end())) + "\"";
+            vsdk_serial += "\n\"SMBIOS\": \"" + Vital::System::Crypto::hash("SHA256", std::string(smbios.serial.begin(), smbios.serial.end())) + "\"";
+            vsdk_serial += "\n\"CPU\": \"" + Vital::System::Crypto::hash("SHA256", std::string(cpu.id.begin(), cpu.id.end())) + "\"";
             for (int i = 0; i < memory.size(); i++) {
-                vsdk_serial += "\n\t\"Memory: " + Vital::System::Crypto::hash("SHA256", std::string(memory.at(i).serial.begin(), memory.at(i).serial.end())) + "\",";
+                vsdk_serial += "\n\"Memory-" + std::to_string(i + 1) + "\": \"" + Vital::System::Crypto::hash("SHA256", std::string(memory.at(i).serial.begin(), memory.at(i).serial.end())) + "\"";
             }
             for (int i = 0; i < network.size(); i++) {
-                if (network.at(i).mac != L"-") vsdk_serial += "\n\t\"Network: " + Vital::System::Crypto::hash("SHA256", std::string(network.at(i).mac.begin(), network.at(i).mac.end())) + "\",";
+                if (network.at(i).mac != L"-") vsdk_serial += "\n\"Network\": \"" + Vital::System::Crypto::hash("SHA256", std::string(network.at(i).mac.begin(), network.at(i).mac.end())) + "\"";
             }
             for (int i = 0; i < disk.size(); i++) {
-                vsdk_serial += "\n\t\"Disk: " + Vital::System::Crypto::hash("SHA256", std::string(disk.at(i).serial.begin(), disk.at(i).serial.end())) + "\",";
+                vsdk_serial += "\n\"Disk\": \"" + Vital::System::Crypto::hash("SHA256", std::string(disk.at(i).serial.begin(), disk.at(i).serial.end())) + "\"";
             }
-            vsdk_serial = vsdk_serial.substr(0, vsdk_serial.size() - 1);
-            vsdk_serial += "\n]";
         }
         if (vsdk_serial.empty()) throw std::runtime_error(ErrorCode["serial-nonexistent"]);
         return vsdk_serial;
