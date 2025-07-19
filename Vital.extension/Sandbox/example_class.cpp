9895e28e2445b24a333aba1dc74d3b2ad53e3d3f
A <103286142+ov-anisa@users.noreply.github.com>
2025-07-19 22:45:08 +0300

    Update example_class.cpp

--- Vital.extension/Sandbox/example_class.cpp
+++ Vital.extension/Sandbox/example_class.cpp
@@ -1,13 +1,13 @@
 #include "example_class.h"
 #include <godot_cpp/variant/utility_functions.hpp>
-#include <../Vital.sandbox/Vendor/lua/lua.hpp>
-
 #include <godot_cpp/classes/scene_tree.hpp>
 #include <godot_cpp/classes/viewport.hpp>
+
+#include <../Vital.sandbox/Sandbox/lua/public/api.h>
 #include <../Vital.sandbox/System/public/vital.h>
 #include <../Vital.sandbox/System/public/crypto.h>
 
-lua_State *L = nullptr;
+Vital::Sandbox::Lua::create* luaVM = nullptr;
 
 ExampleClass::ExampleClass() {
 	UtilityFunctions::print("works?");
@@ -19,36 +19,22 @@ ExampleClass::ExampleClass() {
     auto serial = Vital::System::getSystemSerial();
 	UtilityFunctions::print(serial.c_str());
 
+    luaVM = new Vital::Sandbox::Lua::create();
+	luaVM -> bindAPI();
+	luaVM -> injectAPI();
 
-	L = luaL_newstate();
-
-	// Open standard libraries
-	luaL_openlibs(L);
-
-	// Lua script to run
-	const char *lua_script = R"(
+	std::string rwString = R"(
         local a = 10
         local b = 20
         return a + b
     )";
 
-	// Run the script
-	if (luaL_loadstring(L, lua_script) || lua_pcall(L, 0, 1, 0)) {
-	}
-
-	// The result is on top of the stack
-	if (lua_isnumber(L, -1)) {
-		double result = lua_tonumber(L, -1);
-		std::cout << "Result: " << result << std::endl;
-		UtilityFunctions::print(result);
-	} else {
-		UtilityFunctions::print("unexpected result");
-	}
-
-	auto stuff = Vital::System::Crypto::hash("SHA256", "hello");
+	luaVM -> loadString(rwString);
+	double result = luaVM -> getInt(-1);
+	UtilityFunctions::print(result);
 	
+	auto stuff = Vital::System::Crypto::hash("SHA256", "hello");
 	UtilityFunctions::print(stuff.c_str());
-
 	//ClassDB::register_abstract_class<ExampleClass>();
 
 }
@@ -67,22 +53,13 @@ void ExampleClass::_process(double delta) {
 	//UtilityFunctions::print("rendered");
 	
 	// Lua script to run
-	const char *lua_script = R"(
+	std::string rwString = R"(
         local a = 10
         local b = 20
         return a * b
     )";
 
-	// Run the script
-	if (luaL_loadstring(L, lua_script) || lua_pcall(L, 0, 1, 0)) {
-	}
-
-	// The result is on top of the stack
-	if (lua_isnumber(L, -1)) {
-		double result = lua_tonumber(L, -1);
-		//UtilityFunctions::print(result);
-	}
-
+	luaVM -> loadString(rwString);
 
     /*
         auto *root_node = get_tree()->get_root()->get_node("ExampleClass"); // Gets the root of the scene tree
