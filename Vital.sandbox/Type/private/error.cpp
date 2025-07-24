c5d7c4e245825459f742ba42125e8b831cc2839b
A <89867985+ov-aviril@users.noreply.github.com>
2025-07-25 02:44:23 +0300

    Update error.cpp

--- Vital.sandbox/Type/private/error.cpp
+++ Vital.sandbox/Type/private/error.cpp
@@ -25,11 +25,11 @@ std::map<std::string, std::string> Vital::ErrorCode = {
     {"invalid-arguments", "Invalid argument list"},
     {"invalid-thread", "Invalid thread entity"},
     {"invalid-entities", "Invalid entity list"},
-    {"request-failed", "Failed to process specified request ({})"},
-    {"hash-mode-nonexistent", "Failed to fetch hash mode"},
-    {"cipher-mode-nonexistent", "Failed to fetch cipher mode"},
+    {"request-failed", "Failed to process request ({})"},
+    {"hash-mode-nonexistent", "Failed to fetch hash mode ({})"},
+    {"cipher-mode-nonexistent", "Failed to fetch cipher mode ({})"},
     {"cipher-invalid-key", "Invalid cipher key"},
     {"cipher-invalid-iv", "Invalid cipher IV"},
-    {"file-nonexistent", "Failed to fetch specified file"},
+    {"file-nonexistent", "Failed to fetch file ({})"},
     {"serial-nonexistent", "Failed to fetch device's serial"}
 };
