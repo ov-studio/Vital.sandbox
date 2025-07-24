e045c5a0ca0e1ea85037863dd58c8a510e4c2678
A <89867985+ov-aviril@users.noreply.github.com>
2025-07-24 22:26:01 +0300

    Init REST system module

---
+++ Vital.sandbox/System/private/rest.cpp
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: System: private: rest.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: REST System
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/rest.h>
+#include <curl/curl.h>
+
+
+/////////////////////////
+// Vital: Type: Event //
+/////////////////////////
+
+namespace Vital::System::REST {
+    size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) {
+        size_t totalSize = size*nmemb;
+        userp -> append((char*)contents, totalSize);
+        return totalSize;
+    }
+
+    std::string get(const std::string& url) {
+        CURL* curl;
+        CURLcode res;
+        std::string buffer;
+        curl_global_init(CURL_GLOBAL_DEFAULT);
+        curl = curl_easy_init();
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) throw std::runtime_error(curl_easy_strerror(res))
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+        return buffer;
+    }
+}
---
+++ Vital.sandbox/System/public/rest.h
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: System: public: package.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: REST System
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <System/public/vital.h>
+
+
+/////////////////////////////
+// Vital: System: Package //
+/////////////////////////////
+
+namespace Vital::System::REST {
+    extern std::string get(std::string& url);
+}
