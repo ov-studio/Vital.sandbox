f5ddeb09af5d3b3048facd91998e0040216c9a37
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-15 16:57:33 +0300

    Update lua.cpp

--- Vital.extension/Sandbox/private/lua.cpp
+++ Vital.extension/Sandbox/private/lua.cpp
@@ -1,4 +1,4 @@
-#include "lua.h"
+#include <Sandbox/lua.h>
 #include <../Vital.sandbox/Sandbox/lua/public/api.h>
 #include <../Vital.sandbox/System/public/vital.h>
 #include <../Vital.sandbox/System/public/crypto.h>
