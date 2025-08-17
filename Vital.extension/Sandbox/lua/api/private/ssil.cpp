44adf6fe2748c465b3de3e8415a2638d8e32ca2f
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 11:38:00 +0300

    Update ssil.cpp

--- Vital.extension/Sandbox/lua/api/private/ssil.cpp
+++ Vital.extension/Sandbox/lua/api/private/ssil.cpp
@@ -99,5 +99,24 @@ void Vital::Godot::Sandbox::Lua::API::SSIL::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "set_ssil_normal_rejection", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto rejection = vm -> getInt(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_ssil_normal_rejection(rejection);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "get_ssil_normal_rejection", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_ssil_normal_rejection());
+            return 1;
+        });
+    });
     #endif
 }
