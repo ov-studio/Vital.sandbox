a5e076f1ce9aa8cf53982a2dbcb84d63459eed6e
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 11:47:06 +0300

    Create sdfgi.cpp

---
+++ Vital.extension/Sandbox/lua/api/private/sdfgi.cpp
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: private: sdfgi.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: SDFGI APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/api/public/sdfgi.h>
+
+
+/////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: SDFGI //
+/////////////////////////////////////////////
+
+void Vital::Godot::Sandbox::Lua::API::SDFGI::bind(void* instance) {
+    auto vm = static_cast<Vital::Sandbox::Lua::vsdk_vm*>(instance);
+
+    #if defined(Vital_SDK_Client)
+    Vital::Sandbox::Lua::API::bind(vm, "sdfgi", "setEnabled", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isBool(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto state = vm -> getBool(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_sdfgi_enabled(state);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "sdfgi", "isEnabled", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setBool(Vital::Godot::Engine::Singleton::get_environment() -> is_sdfgi_enabled());
+            return 1;
+        });
+    });
+    #endif
+}
