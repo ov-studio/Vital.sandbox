bbee63f04860e3ffff7b1c44aa29095dcef662c7
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 11:31:47 +0300

    Init SSIL module

---
+++ Vital.extension/Sandbox/lua/api/private/ssil.cpp
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: private: ssil.cpp
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: SSIL APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/api/public/ssil.h>
+
+
+////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: SSIL //
+////////////////////////////////////////////
+
+void Vital::Godot::Sandbox::Lua::API::SSIL::bind(void* instance) {
+    auto vm = static_cast<Vital::Sandbox::Lua::vsdk_vm*>(instance);
+
+    #if defined(Vital_SDK_Client)
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "setEnabled", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isBool(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto state = vm -> getBool(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_ssil_enabled(state);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "isEnabled", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setBool(Vital::Godot::Engine::Singleton::get_environment() -> is_ssil_enabled());
+            return 1;
+        });
+    });
+    #endif
+}
---
+++ Vital.extension/Sandbox/lua/api/public/ssil.h
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: public: ssil.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: SSIL APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/public/api.h>
+
+
+////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: SSIL //
+////////////////////////////////////////////
+
+namespace Vital::Godot::Sandbox::Lua::API {
+    class SSIL : public Vital::Type::Module {
+        public:
+            static void bind(void* instance);
+    };
+}
