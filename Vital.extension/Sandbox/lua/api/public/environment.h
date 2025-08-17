cad5eb0b9fcafb546048a7512235a3dee9c9e61d
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 09:07:40 +0300

    Create environment.h

---
+++ Vital.extension/Sandbox/lua/api/public/environment.h
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: public: environment.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Environment APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.sandbox/Sandbox/lua/public/api.h>
+
+
+///////////////
+// Lua: API //
+///////////////
+
+namespace Vital::Godot::Sandbox::Lua::API {
+    class Environment : public Vital::Type::Module {
+        public:
+            static void bind(void* instance);
+    };
+}
