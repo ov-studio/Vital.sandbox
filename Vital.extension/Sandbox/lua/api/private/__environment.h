0b908901e3e537ffdf80dab331e7242b128bedf1
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 11:44:24 +0300

    Update __environment.h

--- Vital.extension/Sandbox/lua/api/private/__environment.h
+++ Vital.extension/Sandbox/lua/api/private/__environment.h
@@ -108,8 +108,8 @@ public:
 	float get_tonemap_white() const;
 	//void set_ssr_enabled(bool p_enabled);
 	//bool is_ssr_enabled() const;
-	void set_ssr_max_steps(int32_t p_max_steps);
-	int32_t get_ssr_max_steps() const;
+	//void set_ssr_max_steps(int32_t p_max_steps);
+	//int32_t get_ssr_max_steps() const;
 	//void set_ssr_fade_in(float p_fade_in);
 	//float get_ssr_fade_in() const;
 	//void set_ssr_fade_out(float p_fade_out);
