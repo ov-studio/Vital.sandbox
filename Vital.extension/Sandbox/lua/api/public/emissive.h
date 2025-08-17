477f2a6dad84d31922c0114dc573e21527c10d1d
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 15:06:16 +0300

    Create emissive.h

---
+++ Vital.extension/Sandbox/lua/api/public/emissive.h
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: public: emissive.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Emissive APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/public/api.h>
+
+
+////////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: Emissive //
+////////////////////////////////////////////////
+
+namespace Vital::Godot::Sandbox::Lua::API {
+    class Emissive : public Vital::Type::Module {
+        public:
+            static void bind(void* instance);
+    };
+}
