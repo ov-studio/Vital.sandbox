13f54ff30502cc68141e0bb8f33b31f632590456
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 09:47:54 +0300

    Added APIs: `environment.set_bg_energy_multiplier` & `environment.get_bg_energy_multiplier`

--- Vital.extension/Sandbox/lua/api/private/environment.cpp
+++ Vital.extension/Sandbox/lua/api/private/environment.cpp
@@ -42,6 +42,25 @@ void Vital::Godot::Sandbox::Lua::API::Environment::bind(void* instance) {
         });
     });
 
+    Vital::Sandbox::Lua::API::bind(vm, "environment", "set_bg_energy_multiplier", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto energy = vm -> getInt(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_bg_energy_multiplier(energy);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "environment", "get_bg_energy_multiplier", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_bg_energy_multiplier());
+            return 1;
+        });
+    });
+
     Vital::Sandbox::Lua::API::bind(vm, "environment", "get_ssao_intensity", [](auto* ref) -> int {
         auto vm = Vital::Sandbox::Lua::fetchVM(ref);
         return vm -> execute([&]() -> int {
