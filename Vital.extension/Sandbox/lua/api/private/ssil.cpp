9640491e4a8de4d18412e6af052d1d9b35d4ec3f
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 11:34:03 +0300

    Update ssil.cpp

--- Vital.extension/Sandbox/lua/api/private/ssil.cpp
+++ Vital.extension/Sandbox/lua/api/private/ssil.cpp
@@ -61,5 +61,24 @@ void Vital::Godot::Sandbox::Lua::API::SSIL::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "setIntensity", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto intensity = vm -> getInt(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_ssil_intensity(intensity);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "ssil", "getIntensity", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_ssil_intensity());
+            return 1;
+        });
+    });
     #endif
 }
