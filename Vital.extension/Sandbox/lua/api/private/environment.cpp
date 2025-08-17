5da327791ac3ebd073abf5d396cddd0ab139ca2a
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 09:44:24 +0300

    Added APIs: `environment.set_sky_custom_fov` & `environment.get_sky_custom_fov`

--- Vital.extension/Sandbox/lua/api/private/environment.cpp
+++ Vital.extension/Sandbox/lua/api/private/environment.cpp
@@ -23,6 +23,25 @@
 void Vital::Godot::Sandbox::Lua::API::Environment::bind(void* instance) {
     auto vm = static_cast<Vital::Sandbox::Lua::vsdk_vm*>(instance);
 
+    Vital::Sandbox::Lua::API::bind(vm, "environment", "set_sky_custom_fov", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto fov = vm -> getInt(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_sky_custom_fov(fov);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "environment", "get_sky_custom_fov", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_sky_custom_fov());
+            return 1;
+        });
+    });
+
     Vital::Sandbox::Lua::API::bind(vm, "environment", "get_ssao_intensity", [](auto* ref) -> int {
         auto vm = Vital::Sandbox::Lua::fetchVM(ref);
         return vm -> execute([&]() -> int {
