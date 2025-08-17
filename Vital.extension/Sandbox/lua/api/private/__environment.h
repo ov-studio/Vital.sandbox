1301ecddf783db4a623b29546228dbe886ad7e84
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-17 10:22:09 +0300

    Update __environment.h

--- Vital.extension/Sandbox/lua/api/private/__environment.h
+++ Vital.extension/Sandbox/lua/api/private/__environment.h
@@ -130,10 +130,10 @@ public:
 	//float get_ssao_horizon() const;
 	//void set_ssao_sharpness(float p_sharpness);
 	//float get_ssao_sharpness() const;
-	void set_ssao_direct_light_affect(float p_amount);
-	float get_ssao_direct_light_affect() const;
-	void set_ssao_ao_channel_affect(float p_amount);
-	float get_ssao_ao_channel_affect() const;
+	//void set_ssao_direct_light_affect(float p_amount);
+	//float get_ssao_direct_light_affect() const;
+	//void set_ssao_ao_channel_affect(float p_amount);
+	//float get_ssao_ao_channel_affect() const;
 	void set_ssil_enabled(bool p_enabled);
 	bool is_ssil_enabled() const;
 	void set_ssil_radius(float p_radius);
