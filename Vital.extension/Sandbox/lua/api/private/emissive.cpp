713c46a275596e5161e84f3ce5d18d4e4dc4bd79
A <103286142+ov-anisa@users.noreply.github.com>
2025-08-18 11:44:26 +0300

    Update emissive.cpp

--- Vital.extension/Sandbox/lua/api/private/emissive.cpp
+++ Vital.extension/Sandbox/lua/api/private/emissive.cpp
@@ -102,5 +102,24 @@ void Vital::Godot::Sandbox::Lua::API::Emissive::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "emissive", "setStrength", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto intensity = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_glow_strength(intensity);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "emissive", "getStrength", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_glow_strength());
+            return 1;
+        });
+    });
     #endif
 }
