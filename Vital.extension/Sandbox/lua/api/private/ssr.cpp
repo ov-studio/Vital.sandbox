d49712e1b7e55c1dc282f8d07c4473b3cd4c2560
A <103286142+ov-anisa@users.noreply.github.com>
2025-08-18 11:53:23 +0300

    Update ssr.cpp

--- Vital.extension/Sandbox/lua/api/private/ssr.cpp
+++ Vital.extension/Sandbox/lua/api/private/ssr.cpp
@@ -47,8 +47,8 @@ void Vital::Godot::Sandbox::Lua::API::SSR::bind(void* instance) {
         auto vm = Vital::Sandbox::Lua::fetchVM(ref);
         return vm -> execute([&]() -> int {
             if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
-            auto steps = vm -> getInt(1);
-            Vital::Godot::Engine::Singleton::get_environment() -> set_ssr_max_steps(steps);
+            auto amount = vm -> getInt(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_ssr_max_steps(amount);
             vm -> setBool(true);
             return 1;
         });
@@ -104,8 +104,8 @@ void Vital::Godot::Sandbox::Lua::API::SSR::bind(void* instance) {
         auto vm = Vital::Sandbox::Lua::fetchVM(ref);
         return vm -> execute([&]() -> int {
             if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
-            auto tolerance = vm -> getFloat(1);
-            Vital::Godot::Engine::Singleton::get_environment() -> set_ssr_depth_tolerance(tolerance);
+            auto amount = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_ssr_depth_tolerance(amount);
             vm -> setBool(true);
             return 1;
         });
