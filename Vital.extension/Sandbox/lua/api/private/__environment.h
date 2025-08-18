d3b0516f30769648a59ca47188d5a25d2f830662
A <103286142+ov-anisa@users.noreply.github.com>
2025-08-18 11:30:40 +0300

    Update __environment.h

--- Vital.extension/Sandbox/lua/api/private/__environment.h
+++ Vital.extension/Sandbox/lua/api/private/__environment.h
@@ -172,8 +172,8 @@ public:
 	//bool is_glow_enabled() const;
 	//void set_glow_level(int32_t p_idx, float p_intensity);
 	//float get_glow_level(int32_t p_idx) const;
-	void set_glow_normalized(bool p_normalize);
-	bool is_glow_normalized() const;
+	//void set_glow_normalized(bool p_normalize);
+	//bool is_glow_normalized() const;
 	void set_glow_intensity(float p_intensity);
 	float get_glow_intensity() const;
 	void set_glow_strength(float p_strength);
