644f4534527abee7a6586e4c9af5e66c0e9d5b82
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-20 18:06:17 +0300

    Update __environment.h

--- Vital.extension/Sandbox/lua/api/private/__environment.h
+++ Vital.extension/Sandbox/lua/api/private/__environment.h
@@ -183,7 +183,7 @@ public:
 	//void set_glow_bloom(float p_amount);
 	//float get_glow_bloom() const;
 	void set_glow_blend_mode(Environment::GlowBlendMode p_mode);
-	Environment::GlowBlendMode get_glow_blend_mode() const;
+	//Environment::GlowBlendMode get_glow_blend_mode() const;
 	//void set_glow_hdr_bleed_threshold(float p_threshold);
 	//float get_glow_hdr_bleed_threshold() const;
 	//void set_glow_hdr_bleed_scale(float p_scale);
