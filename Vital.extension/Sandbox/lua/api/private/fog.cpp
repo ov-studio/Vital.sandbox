8aaf462a21063c2aaaaa30db01e4ba5a4efe917d
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 09:21:52 +0300

    Update fog.cpp

--- Vital.extension/Sandbox/lua/api/private/fog.cpp
+++ Vital.extension/Sandbox/lua/api/private/fog.cpp
@@ -175,5 +175,24 @@ void Vital::Godot::Sandbox::Lua::API::Fog::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "fog", "setDepthCurve", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto value = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_fog_depth_curve(value);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "fog", "getDepthCurve", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_fog_depth_curve());
+            return 1;
+        });
+    });
     #endif
 }
