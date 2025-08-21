9d4e3620b59f151aea951680d8fdd0de2068b6fc
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 10:15:20 +0300

    Create volumetric_fog.h

---
+++ Vital.extension/Sandbox/lua/api/public/volumetric_fog.h
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: public: volumetric_fog.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Volumetric Fog APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/public/api.h>
+
+
+/////////////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: VolumetricFog //
+/////////////////////////////////////////////////////
+
+namespace Vital::Godot::Sandbox::Lua::API {
+    class VolumetricFog : public Vital::Type::Module {
+        public:
+            static void bind(void* instance);
+    };
+}
