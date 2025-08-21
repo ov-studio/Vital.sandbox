91a31c74e7011207daf928a9ed565099096fa7b7
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 08:49:50 +0300

    Update fog.cpp

--- Vital.extension/Sandbox/lua/api/private/fog.cpp
+++ Vital.extension/Sandbox/lua/api/private/fog.cpp
@@ -61,5 +61,24 @@ void Vital::Godot::Sandbox::Lua::API::Fog::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "fog", "setSunScatter", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto value = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_fog_sun_scatter(value);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "fog", "getSunScatter", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_fog_sun_scatter());
+            return 1;
+        });
+    });
     #endif
 }
