4437617832a9146086073a507c7ea56e005a2d80
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 10:50:04 +0300

    Create adjustment.h

---
+++ Vital.extension/Sandbox/lua/api/public/adjustment.h
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: lua: api: public: adjustment.h
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Adjustment APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/lua/public/api.h>
+
+
+//////////////////////////////////////////////////
+// Vital: Godot: Sandbox: Lua: API: Adjustment //
+//////////////////////////////////////////////////
+
+namespace Vital::Godot::Sandbox::Lua::API {
+    class Adjustment : public Vital::Type::Module {
+        public:
+            static void bind(void* instance);
+    };
+}
