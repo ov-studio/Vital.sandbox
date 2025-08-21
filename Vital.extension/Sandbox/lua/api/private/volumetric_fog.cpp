af125f50c06a4ecfeef957769c1d2b4b8ed5c110
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 10:30:21 +0300

    Update volumetric_fog.cpp

--- Vital.extension/Sandbox/lua/api/private/volumetric_fog.cpp
+++ Vital.extension/Sandbox/lua/api/private/volumetric_fog.cpp
@@ -192,5 +192,24 @@ void Vital::Godot::Sandbox::Lua::API::VolumetricFog::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "volumetric_fog", "setDetailSpread", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto value = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_volumetric_fog_detail_spread(value);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "volumetric_fog", "getDetailSpread", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_volumetric_fog_detail_spread());
+            return 1;
+        });
+    });
     #endif
 }
