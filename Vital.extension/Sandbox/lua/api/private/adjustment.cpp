5f7140fc0ff75def33ecb6f002c06feca0dd0f02
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 10:55:08 +0300

    Update adjustment.cpp

--- Vital.extension/Sandbox/lua/api/private/adjustment.cpp
+++ Vital.extension/Sandbox/lua/api/private/adjustment.cpp
@@ -61,5 +61,24 @@ void Vital::Godot::Sandbox::Lua::API::Adjustment::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "adjustment", "setContrast", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto value = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_adjustment_contrast(value);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "adjustment", "getContrast", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_adjustment_contrast());
+            return 1;
+        });
+    });
     #endif
 }
