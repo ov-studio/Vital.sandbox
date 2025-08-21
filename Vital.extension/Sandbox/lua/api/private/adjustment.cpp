bd9f8a85a4a8d5a80f2882f09d8fab507ddf2a0a
A <89867985+ov-aviril@users.noreply.github.com>
2025-08-21 10:55:46 +0300

    Update adjustment.cpp

--- Vital.extension/Sandbox/lua/api/private/adjustment.cpp
+++ Vital.extension/Sandbox/lua/api/private/adjustment.cpp
@@ -80,5 +80,24 @@ void Vital::Godot::Sandbox::Lua::API::Adjustment::bind(void* instance) {
             return 1;
         });
     });
+
+    Vital::Sandbox::Lua::API::bind(vm, "adjustment", "setSaturation", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            if ((vm -> getArgCount() < 1) || (!vm -> isNumber(1))) throw std::runtime_error(ErrorCode["invalid-arguments"]);
+            auto value = vm -> getFloat(1);
+            Vital::Godot::Engine::Singleton::get_environment() -> set_adjustment_saturation(value);
+            vm -> setBool(true);
+            return 1;
+        });
+    });
+
+    Vital::Sandbox::Lua::API::bind(vm, "adjustment", "getSaturation", [](auto* ref) -> int {
+        auto vm = Vital::Sandbox::Lua::fetchVM(ref);
+        return vm -> execute([&]() -> int {
+            vm -> setNumber(Vital::Godot::Engine::Singleton::get_environment() -> get_adjustment_saturation());
+            return 1;
+        });
+    });
     #endif
 }
