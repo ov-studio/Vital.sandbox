0b94d4f6dc4c95852bddde70afec794e3a348dbc
A <89867985+ov-aviril@users.noreply.github.com>
2026-01-07 00:45:48 +0300

    Update canvas.cpp

--- Vital.extension/Engine/private/canvas.cpp
+++ Vital.extension/Engine/private/canvas.cpp
@@ -103,7 +103,7 @@ namespace Vital::Godot {
                         payload.font_size
                     );
                     break;
-                } 
+                }
             }
         }
     };
