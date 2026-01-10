c0981ed818121dc45eb6584c24b6aa44e269faed
A <89867985+ov-aviril@users.noreply.github.com>
2026-01-10 03:33:21 +0300

    Made event header only

--- Vital.sandbox/Tool/event.h
+++ Vital.sandbox/Tool/event.h
@@ -13,20 +13,43 @@
 //////////////
 
 #pragma once
-#include <Vital.sandbox/Tool/index.h>
-#include <Vital.sandbox/Tool/stack.h>
+#include <Vital.sandbox/System/public/vital.h>
 
 
 /////////////////////////
 // Vital: Tool: Event //
 /////////////////////////
 
-namespace Vital::Tool::Event {
-    typedef unsigned long ID;
+namespace Vital::System::Event {
+    using EventID = unsigned long;
+    using Handler = std::function<void(Vital::Tool::Stack)>;
+    using EventMap = std::map<EventID, Handler>;
+    using EventPool = std::map<std::string, EventMap>;
+    inline EventID id = 0;
+    inline EventPool pool;
 
-    typedef struct {
-        const std::function<void()> unbind;
-    } Handle;
+    inline EventID bind(const std::string& identifier, Handler exec) {
+        ++id;
+        const EventID id = id;
+        pool[identifier].emplace(id, std::move(exec));
+        return id;
+    }
 
-    typedef std::function<void(Vital::Tool::Stack)> Handler;
+    inline bool unbind(const std::string& identifier, EventID id) {
+        auto it = pool.find(identifier);
+        if (it == pool.end()) return false;
+        it -> second.erase(id);
+        if (it -> second.empty()) pool.erase(it);
+        return true;
+    }
+
+    inline bool emit(const std::string& identifier, Vital::Tool::Stack arguments = {}) {
+        auto it = pool.find(identifier);
+        if (it == pool.end()) return false;
+        for (const auto& [id, handler] : it -> second) {
+            if (handler)
+                handler(arguments);
+        }
+        return true;
+    }
 }
