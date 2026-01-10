6c39e76d45400ceb273a603923a33ab479d49f6e
A <89867985+ov-aviril@users.noreply.github.com>
2026-01-11 00:14:54 +0300

    Added method to get exe's base directory

--- Vital.sandbox/Tool/index.h
+++ Vital.sandbox/Tool/index.h
@@ -14,6 +14,7 @@
 
 #pragma once
 #include <pch.h>
+#include <godot_cpp/classes/os.hpp>
 #include <Vital.sandbox/Tool/rest.h>
 #include <Vital.sandbox/Vendor/rapidjson/document.h>
 
@@ -29,18 +30,6 @@ namespace Vital {
     static const std::string Signature = "vsdk_v.0.0.1";
     static const std::string Repository = "https://raw.githubusercontent.com/ov-studio/Vital.sandbox/refs/heads/module/{}";
 
-    inline std::vector<std::string> get_modules(const std::string& name) {
-        std::vector<std::string> result;
-        rapidjson::Document manifest;
-        manifest.Parse(Vital::System::Rest::get(fmt::format(Repository, "manifest.json")).c_str());
-        if (!manifest.HasParseError() && manifest.HasMember(name.c_str())) {
-            for (auto& i : manifest[name.c_str()]["sources"].GetArray()) {
-                result.push_back(Vital::System::Rest::get(fmt::format(Repository, name + "/" + std::string(i.GetString()))));
-            }
-        }
-        return result;
-    }
-
     inline const std::string get_platform() { 
         #if defined(Vital_SDK_Client)
             return "client";
@@ -51,5 +40,21 @@ namespace Vital {
 
     inline unsigned int get_tick() {
         return static_cast<unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()/1000000);
+    }
+
+    inline godot::String get_directory() {
+        return godot::OS::get_singleton() -> get_executable_path().get_base_dir();
+    }
+
+    inline std::vector<std::string> get_modules(const std::string& name) {
+        std::vector<std::string> result;
+        rapidjson::Document manifest;
+        manifest.Parse(Vital::System::Rest::get(fmt::format(Repository, "manifest.json")).c_str());
+        if (!manifest.HasParseError() && manifest.HasMember(name.c_str())) {
+            for (auto& i : manifest[name.c_str()]["sources"].GetArray()) {
+                result.push_back(Vital::System::Rest::get(fmt::format(Repository, name + "/" + std::string(i.GetString()))));
+            }
+        }
+        return result;
     }
 }
