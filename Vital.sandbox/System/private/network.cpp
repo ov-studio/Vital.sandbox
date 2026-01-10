3422f62cf0ae3e30fad2b6abd496254ec9d5deaa
A <89867985+ov-aviril@users.noreply.github.com>
2026-01-10 23:18:53 +0300

    Update network.cpp

--- Vital.sandbox/System/private/network.cpp
+++ Vital.sandbox/System/private/network.cpp
@@ -14,7 +14,7 @@
 
 #pragma once
 #include <Vital.sandbox/System/public/network.h>
-#include <Vital.sandbox/System/public/crypto.h>
+#include <Vital.sandbox/Tool/crypto.h>
 #include <Vital.sandbox/Tool/event.h>
 #include <Vital.sandbox/Vendor/enet/enet.h>
 
