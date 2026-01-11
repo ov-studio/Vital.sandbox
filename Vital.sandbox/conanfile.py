6994f7d37511bcf494cd87b5a64a3ce6e72d3fbe
Jizdan Ianec <jizdan.ianec17@gmail.com>
2026-01-11 23:27:58 +0200

    fixed the RapidJSON error, fixed the ENet error by preventing redefinition of socklen_t

--- Vital.sandbox/conanfile.py
+++ Vital.sandbox/conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.tools.scons import SConsDeps
 
 class BuildConan(ConanFile):
-    settings = "os", "arch","build_type", "compiler"
+    settings = "os", "arch", "build_type", "compiler"
     generators = "SConsDeps"
 
     def requirements(self):
@@ -12,5 +12,9 @@ class BuildConan(ConanFile):
     def configure(self):
         self.options["openssl"].shared = False
         self.options["libcurl"].shared = False
-        self.settings.compiler.cppstd = "17"
-        self.settings.compiler.runtime = "static"
+
+        if self.settings.compiler.get_safe("cppstd"):
+            self.settings.compiler.cppstd = "17"
+
+        if self.settings.compiler == "msvc":
+            self.settings.compiler.runtime = "static"
