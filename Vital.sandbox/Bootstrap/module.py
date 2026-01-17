4c5eec09a8a648f7d9b9907fa6e82e3f4ebec0fd
A <103286142+ov-anisa@users.noreply.github.com>
2026-01-18 02:55:22 +0300

    Staged all files into Vital.%platform_type%

--- Vital.sandbox/Bootstrap/module.py
+++ Vital.sandbox/Bootstrap/module.py
@@ -11,7 +11,12 @@ def Build_Module(self):
 BaseEnvironment.Build_Module = Build_Module
 
 def Stage_Module(self, build):
+    cwd = os.path.abspath(os.getcwd())
+    vital_bin = os.path.normpath(os.path.join(cwd, "..", f"Vital.{self.Args["platform_type"].lower()}", self.name.lower(), self["platform"]))
     build_dir = os.path.dirname(str(build[0].abspath))
+    vilta_stage = self.Alias("stage", self.Install(vital_bin, self.Glob(os.path.join(build_dir, "*"))))
     self.Stage_VCPKG(build, build_dir)
     self.Stage_Discord(build, build_dir)
+    self.Depends(vilta_stage, build)
+    self.Default(vilta_stage)
 BaseEnvironment.Stage_Module = Stage_Module
