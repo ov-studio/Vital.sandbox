9e92c510a5980e43477635738919fc5a2f83428c
A <103286142+ov-anisa@users.noreply.github.com>
2026-01-19 21:21:38 +0300

    Merged rest sources

--- Vital.sandbox/Sandbox/lua/api/public/rest.h
+++ Vital.sandbox/Sandbox/lua/api/public/rest.h
@@ -23,6 +23,30 @@
 namespace Vital::Sandbox::Lua::API {
     class Rest : public Vital::Tool::Module {
         public:
-            static void bind(void* instance);
+            inline static void bind(void* instance) {
+                auto vm = Vital::Sandbox::Lua::toVM(instance);
+
+                API::bind(vm, "rest", "get", [](auto* ref) -> int {
+                    auto vm = fetchVM(ref);
+                    return vm -> execute([&]() -> int {
+                        if (!vm -> isVirtualThread()) throw Vital::Error::fetch("invalid-thread");
+                        if ((vm -> getArgCount() < 1) || (!vm -> isString(1))) throw Vital::Error::fetch("invalid-arguments");
+                        auto url = vm -> getString(1);
+                        Vital::Tool::Thread([=](Vital::Tool::Thread* thread) -> void {
+                            try {
+                                vm -> setString(Vital::System::Rest::get(url));
+                                vm -> setBool(false);
+                            }
+                            catch(const std::runtime_error& error) {
+                                vm -> setBool(false);
+                                vm -> setString(error.what());
+                            }
+                            vm -> resume(2);
+                        }).detach();
+                        vm -> pause();
+                        return 0;
+                    });
+                });
+            }
     };
 }
