8cb01d968f70fcd38e1e3fa49fa3a428a69d2c3a
A <103286142+ov-anisa@users.noreply.github.com>
2026-01-20 04:55:49 +0300

    .

--- Vital.extension/Sandbox/lua/private/index.cpp
+++ Vital.extension/Sandbox/lua/private/index.cpp
@@ -54,7 +54,7 @@ namespace Vital::Godot::Sandbox::Lua {
     Singleton::Singleton() {
         godot::UtilityFunctions::print("Initialized Lua vm");
 
-        vm = new Vital::Sandbox::Lua::create({
+        vm = new Vital::Sandbox::Lua::Machine({
             {API::SSR::bind, API::SSR::inject},
             {API::SSAO::bind, API::SSAO::inject},
             {API::SSIL::bind, API::SSIL::inject},
