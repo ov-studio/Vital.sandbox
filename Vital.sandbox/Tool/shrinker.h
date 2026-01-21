c89d959c0f92bf295389ada80184d12acb9d8cd7
A <89867985+ov-aviril@users.noreply.github.com>
2026-01-21 04:44:51 +0300

    Update shrinker.h

--- Vital.sandbox/Tool/shrinker.h
+++ Vital.sandbox/Tool/shrinker.h
@@ -25,45 +25,25 @@ namespace Vital::Tool::Shrinker {
     inline std::string compress(const std::string& input) {
         uLong src_size = static_cast<uLong>(input.size());
         uLong dest_size = compressBound(src_size);
-    
-        std::string out;
-        out.resize(dest_size);
-    
-        int res = compress2(
-            reinterpret_cast<Bytef*>(&out[0]),
-            &dest_size,
-            reinterpret_cast<const Bytef*>(input.data()),
-            src_size,
-            Z_BEST_COMPRESSION
-        );
-    
-        if (res != Z_OK)
-            throw std::runtime_error("zlib compression failed");
-    
-        out.resize(dest_size);
-        return out;
+        std::string output;
+        output.resize(dest_size);
+        int res = compress2(reinterpret_cast<Bytef*>(&output[0]), &dest_size, reinterpret_cast<const Bytef*>(input.data()), src_size, Z_BEST_COMPRESSION);
+        if (res != Z_OK) throw std::runtime_error("zlib compression failed");
+        output.resize(dest_size);
+        return output;
     }
     
     inline std::string decompress(const std::string& input) {
         uLong dest_size = input.size()*4;
-        std::string out;
-        out.resize(dest_size);
-
+        std::string output;
+        output.resize(dest_size);
         int res;
-        while ((res = uncompress(
-            reinterpret_cast<Bytef*>(&out[0]),
-            &dest_size,
-            reinterpret_cast<const Bytef*>(input.data()),
-            static_cast<uLong>(input.size())
-        )) == Z_BUF_ERROR) {
+        while ((res = uncompress(reinterpret_cast<Bytef*>(&output[0]), &dest_size, reinterpret_cast<const Bytef*>(input.data()), static_cast<uLong>(input.size()))) == Z_BUF_ERROR) {
             dest_size *= 2;
-            out.resize(dest_size);
+            output.resize(dest_size);
         }
-    
-        if (res != Z_OK)
-            throw std::runtime_error("zlib decompression failed");
-    
-        out.resize(dest_size);
-        return out;
+        if (res != Z_OK) throw std::runtime_error("zlib decompression failed");
+        output.resize(dest_size);
+        return output;
     }
 }
