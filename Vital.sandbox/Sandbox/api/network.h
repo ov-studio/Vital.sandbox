7818a8942f02ca3c86fcefbcaffcf3ab89f2a2df
A <89867985+ov-aviril@users.noreply.github.com>
2026-02-01 02:27:58 +0300

    Added vardaic args support to `Vital::Sandbox::API::Network::execute(...)`

--- Vital.sandbox/Sandbox/api/network.h
+++ Vital.sandbox/Sandbox/api/network.h
@@ -70,13 +70,25 @@ namespace Vital::Sandbox::API {
                 vm -> table_set_nil("execute", "network");
             }
 
-            static void execute(const std::string& name, const std::string& payload) {
+            template<typename... Args>
+            static void execute(const std::string& name, Args&&... args) {
                 for (auto vm : Machine::fetch_buffer()) {
                     if (!vm.second -> is_virtual()) {
                         vm.second -> get_reference("vital.network:execute", true);
                         vm.second -> push_string(name);
-                        vm.second -> push_string(payload);
-                        vm.second -> pcall(2, 0);
+                        ([&vm](auto&& arg) {
+                            using T = std::decay_t<decltype(arg)>;
+                            if constexpr (std::is_null_pointer_v<T>) vm.second -> push_nil();
+                            else if constexpr (std::is_same_v<T, bool>) vm.second -> push_bool(arg);
+                            else if constexpr (std::is_same_v<T, int>) vm.second -> push_number(arg);
+                            else if constexpr (std::is_same_v<T, float>) vm.second -> push_number(arg);
+                            else if constexpr (std::is_same_v<T, double>) vm.second -> push_number(arg);
+                            else if constexpr (std::is_same_v<T, const char*> || std::is_same_v<T, char*>) vm.second -> push_string(std::string(arg));
+                            else if constexpr (std::is_same_v<T, std::string>) vm.second -> push_string(arg);
+                            else if constexpr (std::is_pointer_v<T> && !std::is_same_v<T, const char*>) vm.second -> push_userdata(static_cast<void*>(const_cast<std::remove_const_t<std::remove_pointer_t<T>>*>(arg)));
+                            else if constexpr (std::is_same_v<T, vm_exec>) vm.second -> push_function(arg);
+                        }(std::forward<Args>(args)), ...);
+                        vm.second -> pcall(sizeof...(Args) + 1, 0);
                     }
                 }
             }
