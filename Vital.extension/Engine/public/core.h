46f00edcfe6a11778d60bf6e44922e92be57e437
A <103286142+ov-anisa@users.noreply.github.com>
2026-02-11 04:48:33 +0300

    Update core.h

--- Vital.extension/Engine/public/core.h
+++ Vital.extension/Engine/public/core.h
@@ -21,43 +21,30 @@
 ///////////////////////////
 
 namespace Vital::Godot {
-	class Core;
-	class Core : public godot::Node {
-		GDCLASS(Core, godot::Node)
-		protected:
-			inline static Core* singleton = nullptr;
-			inline static godot::WorldEnvironment* environment = nullptr;
-			static void _bind_methods() {};
-		public:
-			// Instantiators //
-			Core() = default;
-			~Core() override = default;
-			void _ready() override;
-			void _exit_tree() override;
-			void _process(double delta) override;
-			void _unhandled_input(godot::Ref<godot::InputEvent> event);
+    class Core;
+    class Core : public godot::Node {
+        GDCLASS(Core, godot::Node)
+        protected:
+            inline static Core* singleton = nullptr;
+            inline static godot::WorldEnvironment* environment = nullptr;
+            static void _bind_methods() {};
+        public:
+            // Instantiators //
+            Core() = default;
+            ~Core() override = default;
+            void _ready() override;
+            void _exit_tree() override;
+            void _process(double delta) override;
+            void _unhandled_input(godot::Ref<godot::InputEvent> event);
 
 
-			// Getters //
-			static Core* get_singleton();
-			static godot::Node* get_root();
-			#if defined(Vital_SDK_Client)
-			static godot::RenderingServer* get_rendering_server();
-			static godot::Ref<godot::Environment> get_environment();
-			static void free_environment();
-			#endif
-
-	
-			// Utils //
-			template <typename T>
-			static void fetch_nodes(godot::Node* node, std::vector<T*> &result, int limit = -1) {
-				if (!node || (limit == 0)) return;
-				T* typed_node = godot::Object::cast_to<T>(node);
-				if (typed_node) result.push_back(typed_node);
-				for (int i = 0; i < node -> get_child_count(); i++) {
-					if ((limit == -1) || (result.size() < limit)) fetch_nodes(node -> get_child(i), result, limit);
-					else break;
-				}
-			}
-	};
+            // Getters //
+            static Core* get_singleton();
+            static godot::Node* get_root();
+            #if defined(Vital_SDK_Client)
+            static godot::RenderingServer* get_rendering_server();
+            static godot::Ref<godot::Environment> get_environment();
+            static void free_environment();
+            #endif
+    };
 }
