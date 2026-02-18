8c31cb39bb6cd330891f8a8a68f81d6e04753b7b
A <89867985+ov-aviril@users.noreply.github.com>
2026-02-18 15:20:35 +0300

    Update model.cpp

--- Vital.extension/Engine/private/model.cpp
+++ Vital.extension/Engine/private/model.cpp
@@ -100,7 +100,7 @@ namespace Vital::Godot {
     std::vector<std::string> Model::get_animations() {
         std::vector<std::string> animations;
         if (!animation_player) return animations;
-        godot::PackedStringArray anim_list = animation_player->get_animation_list();
+        auto anim_list = animation_player -> get_animation_list();
         for (int i = 0; i < anim_list.size(); i++) {
             animations.push_back(to_std_string(anim_list[i]));
         }
