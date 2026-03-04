ed3c85216216caacdd06173411e39a43979b57cf
A <48655846+ov-tron@users.noreply.github.com>
2026-03-04 18:50:48 +0300

    Update model.h

--- Vital.extension/Sandbox/api/model.h
+++ Vital.extension/Sandbox/api/model.h
@@ -31,25 +31,30 @@ namespace Vital::Sandbox::API {
 
             API::bind(vm, {base_name}, "is_loaded", [](auto vm) -> int {
                 if ((vm -> get_arg_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                vm -> push_bool(Vital::Godot::Model::is_model_loaded(vm -> get_string(1)));
+                auto name = vm -> get_string(1);
+                vm -> push_bool(Vital::Godot::Model::is_model_loaded(name));
                 return 1;
             });
 
             API::bind(vm, {base_name}, "load", [](auto vm) -> int {
                 if ((vm -> get_arg_count() < 2) || (!vm -> is_string(1)) || (!vm -> is_string(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                vm -> push_bool(Vital::Godot::Model::load_model(vm -> get_string(1), vm -> get_string(2)));
+                auto name = vm -> get_string(1);
+                auto path = vm -> get_string(2);
+                vm -> push_bool(Vital::Godot::Model::load_model(name, path));
                 return 1;
             });
 
             API::bind(vm, {base_name}, "unload", [](auto vm) -> int {
                 if ((vm -> get_arg_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                vm -> push_bool(Vital::Godot::Model::unload_model(vm -> get_string(1)));
+                auto name = vm -> get_string(1);
+                vm -> push_bool(Vital::Godot::Model::unload_model(name));
                 return 1;
             });
 
             API::bind(vm, {base_name}, "create", [](auto vm) -> int {
                 if ((vm -> get_arg_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                auto* object = Vital::Godot::Model::create_object(vm -> get_string(1));
+                auto name = vm -> get_string(1);
+                auto* object = Vital::Godot::Model::create_object(name);
                 vm -> create_object(base_name, object);
                 return 1;
             });
@@ -58,7 +63,8 @@ namespace Vital::Sandbox::API {
         static void methods(Machine* vm) {
             vm_module::bind_method<base_class>(vm, base_name, "is_component_visible", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 2) || (!vm -> is_string(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                vm -> push_bool(self -> is_component_visible(vm -> get_string(2)));
+                auto name = vm -> get_string(2);
+                vm -> push_bool(self -> is_component_visible(name));
                 return 1;
             });
 
@@ -69,35 +75,47 @@ namespace Vital::Sandbox::API {
 
             vm_module::bind_method<base_class>(vm, base_name, "set_position", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 4) || (!vm -> is_number(2)) || (!vm -> is_number(3)) || (!vm -> is_number(4))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                self -> set_position({vm -> get_float(2), vm -> get_float(3), vm -> get_float(4)});
+                auto x = vm -> get_float(2);
+                auto y = vm -> get_float(3);
+                auto z = vm -> get_float(4);
+                self -> set_position({x, y, z});
                 vm -> push_bool(true);
                 return 1;
             });
 
             vm_module::bind_method<base_class>(vm, base_name, "set_rotation", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 4) || (!vm -> is_number(2)) || (!vm -> is_number(3)) || (!vm -> is_number(4))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                self -> set_rotation({vm -> get_float(2), vm -> get_float(3), vm -> get_float(4)});
+                auto x = vm -> get_float(2);
+                auto y = vm -> get_float(3);
+                auto z = vm -> get_float(4);
+                self -> set_rotation({x, y, z});
                 vm -> push_bool(true);
                 return 1;
             });
 
             vm_module::bind_method<base_class>(vm, base_name, "set_component_visible", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 3) || (!vm -> is_string(2)) || (!vm -> is_bool(3))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                self -> set_component_visible(vm -> get_string(2), vm -> get_bool(3));
+                auto name  = vm -> get_string(2);
+                auto state = vm -> get_bool(3);
+                self -> set_component_visible(name, state);
                 vm -> push_bool(true);
                 return 1;
             });
 
             vm_module::bind_method<base_class>(vm, base_name, "set_animation_speed", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 2) || (!vm -> is_number(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                self -> set_animation_speed(vm -> get_float(2));
+                auto speed = vm -> get_float(2);
+                self -> set_animation_speed(speed);
                 vm -> push_bool(true);
                 return 1;
             });
 
             vm_module::bind_method<base_class>(vm, base_name, "set_blend_shape_value", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 4) || (!vm -> is_string(2)) || (!vm -> is_string(3)) || (!vm -> is_number(4))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                self -> set_blend_shape_value(vm -> get_string(2), vm -> get_string(3), vm -> get_float(4));
+                auto component   = vm -> get_string(2);
+                auto blend_shape = vm -> get_string(3);
+                auto value       = vm -> get_float(4);
+                self -> set_blend_shape_value(component, blend_shape, value);
                 vm -> push_bool(true);
                 return 1;
             });
@@ -145,7 +163,8 @@ namespace Vital::Sandbox::API {
 
             vm_module::bind_method<base_class>(vm, base_name, "get_blend_shapes", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 2) || (!vm -> is_string(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                auto list = self -> get_blend_shapes(vm -> get_string(2));
+                auto component = vm -> get_string(2);
+                auto list = self -> get_blend_shapes(component);
                 vm -> create_table();
                 for (int i = 0; i < (int)list.size(); i++) {
                     vm -> push_string(list[i]);
@@ -166,15 +185,18 @@ namespace Vital::Sandbox::API {
 
             vm_module::bind_method<base_class>(vm, base_name, "get_blend_shape_value", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 3) || (!vm -> is_string(2)) || (!vm -> is_string(3))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                vm -> push_number(self -> get_blend_shape_value(vm -> get_string(2), vm -> get_string(3)));
+                auto component   = vm -> get_string(2);
+                auto blend_shape = vm -> get_string(3);
+                vm -> push_number(self -> get_blend_shape_value(component, blend_shape));
                 return 1;
             });
 
             vm_module::bind_method<base_class>(vm, base_name, "play_animation", [](auto vm, auto self) -> int {
                 if ((vm -> get_arg_count() < 2) || (!vm -> is_string(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
-                bool loop   = vm -> is_bool(3)   ? vm -> get_bool(3)  : true;
-                float speed = vm -> is_number(4) ? vm -> get_float(4) : 1.0f;
-                vm -> push_bool(self -> play_animation(vm -> get_string(2), loop, speed));
+                auto name  = vm -> get_string(2);
+                auto loop  = vm -> is_bool(3)   ? vm -> get_bool(3)  : true;
+                auto speed = vm -> is_number(4) ? vm -> get_float(4) : 1.0f;
+                vm -> push_bool(self -> play_animation(name, loop, speed));
                 return 1;
             });
 
