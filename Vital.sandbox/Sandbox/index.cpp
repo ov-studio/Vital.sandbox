dcda543f5a76ff747f2459ac6185d07b058b25ce
A <48655846+ov-tron@users.noreply.github.com>
2026-03-06 21:35:05 +0300

    Update index.cpp

--- Vital.sandbox/Sandbox/index.cpp
+++ Vital.sandbox/Sandbox/index.cpp
@@ -25,13 +25,13 @@
 namespace Vital::Sandbox {
     vm_apis Machine::internal_apis = {
         vm_module::make_api<API::Engine>(),
+        vm_module::make_api<API::Discord>(),
         vm_module::make_api<API::Coroutine>(),
         vm_module::make_api<API::Network>(),
         vm_module::make_api<API::Rest>(),
         vm_module::make_api<API::File>(),
         vm_module::make_api<API::Crypto>(),
-        vm_module::make_api<API::Shrinker>(),
-        vm_module::make_api<API::Discord>()
+        vm_module::make_api<API::Shrinker>()
     };
 
     namespace API {
