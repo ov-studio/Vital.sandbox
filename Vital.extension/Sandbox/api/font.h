73649600f1654db6b5db8e8b095bc0e7879dbe15
A <89867985+ov-aviril@users.noreply.github.com>
2026-03-10 10:27:05 +0300

    Exposed font apis to lua

---
+++ Vital.extension/Sandbox/api/font.h
@@ -0,0 +1,74 @@
+/*----------------------------------------------------------------
+     Resource: Vital.extension
+     Script: Sandbox: api: font.h
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Font APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.extension/Sandbox/index.h>
+#include <Vital.extension/Engine/public/font.h>
+
+
+////////////////////////////////
+// Vital: Sandbox: API: Font //
+////////////////////////////////
+
+namespace Vital::Sandbox::API {
+    struct Font : vm_module {
+        inline static const std::string base_name = "font";
+        using base_class = Vital::Engine::Font;
+
+        static void bind(Machine* vm) {
+            vm_module::register_type<Font>(vm, base_name);
+
+            API::bind(vm, {base_name}, "create", [](auto vm) -> int {
+                if ((vm -> get_arg_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+                auto path = vm -> get_string(1);
+                auto* object = Vital::Engine::Font::create(path);
+                vm -> create_object(base_name, object);
+                return 1;
+            });
+        }
+
+        static void methods(Machine* vm) {
+            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self) -> int {
+                self -> destroy();
+                vm_module::release_userdata(vm, 1);
+                vm -> push_bool(true);
+                return 1;
+            });
+
+            vm_module::bind_method<base_class>(vm, base_name, "set_antialiasing", [](auto vm, auto self) -> int {
+                if ((vm -> get_arg_count() < 2) || (!vm -> is_bool(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+                self -> set_antialiasing(vm -> get_bool(2));
+                vm -> push_bool(true);
+                return 1;
+            });
+
+            vm_module::bind_method<base_class>(vm, base_name, "get_antialiasing", [](auto vm, auto self) -> int {
+                vm -> push_bool(self -> get_antialiasing());
+                return 1;
+            });
+
+            vm_module::bind_method<base_class>(vm, base_name, "set_oversampling", [](auto vm, auto self) -> int {
+                if ((vm -> get_arg_count() < 2) || (!vm -> is_number(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+                self -> set_oversampling(static_cast<float>(vm -> get_number(2)));
+                vm -> push_bool(true);
+                return 1;
+            });
+
+            vm_module::bind_method<base_class>(vm, base_name, "get_oversampling", [](auto vm, auto self) -> int {
+                vm -> push_number(self -> get_oversampling());
+                return 1;
+            });
+        }
+    };
+}
