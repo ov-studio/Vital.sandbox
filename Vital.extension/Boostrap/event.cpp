dc2b0d3c18faf91218b029d841733405deda8188
A <89867985+ov-aviril@users.noreply.github.com>
2026-03-17 19:11:17 +0300

    Update event.cpp

--- Vital.extension/Boostrap/event.cpp
+++ Vital.extension/Boostrap/event.cpp
@@ -21,69 +21,74 @@
 /////////////
 
 void shutdown() {
-    auto* net = Vital::Engine::Network::get_singleton();
-    if (Vital::Engine::Network::is_server()) net->close();
-    else net->disconnect_from_server();
+    #if !defined(Vital_SDK_Client)
+    Vital::Engine::Network::get_singleton()->close();
+    #endif
+    #if defined(Vital_SDK_Client)
+    Vital::Engine::Network::get_singleton()->disconnect_from_server();
+    #endif
     Vital::Engine::Network::free_singleton();
 }
 
 void setup() {
     auto* net = Vital::Engine::Network::get_singleton();
 
-    if (Vital::Engine::Network::is_server()) {
-        Vital::Tool::Event::bind("network:hosted", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Server is live");
-        });
-        Vital::Tool::Event::bind("network:peer_joined", [](Vital::Tool::Stack& args) {
-            int32_t id = args.array[0].as<int32_t>();
-            Vital::print("sbox", "Player joined: ", id);
-        });
-        Vital::Tool::Event::bind("network:peer_left", [](Vital::Tool::Stack& args) {
-            int32_t id = args.array[0].as<int32_t>();
-            Vital::print("sbox", "Player left: ", id);
-        });
-        Vital::Tool::Event::bind("network:packet", [net](Vital::Tool::Stack& args) {
-            int32_t sender = args.object.at("sender_id").as<int32_t>();
-            std::string msg = args.array[0].as<std::string>();
-            Vital::print("sbox", "Msg from ", sender, ": ", msg.c_str());
-            Vital::Tool::Stack reply;
-            reply.array.push_back(Vital::Tool::StackValue(std::string("pong")));
-            net->send(reply, sender);
-        });
-        Vital::Tool::Event::bind("network:closed", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Server closed");
-        });
-    } else {
-        Vital::Tool::Event::bind("network:connecting", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Connecting...");
-        });
-        Vital::Tool::Event::bind("network:connected", [net](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Connected! My ID: ", net->get_peer_id());
-            Vital::Tool::Stack msg;
-            msg.array.push_back(Vital::Tool::StackValue(std::string("ping")));
-            net->send_to_server(msg);
-        });
-        Vital::Tool::Event::bind("network:packet", [](Vital::Tool::Stack& args) {
-            std::string msg = args.array[0].as<std::string>();
-            Vital::print("sbox", "Server replied: ", msg.c_str());
-        });
-        Vital::Tool::Event::bind("network:connection_failed", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Failed to connect");
-        });
-        Vital::Tool::Event::bind("network:reconnecting", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Retrying...");
-        });
-        Vital::Tool::Event::bind("network:reconnect_failed", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Gave up reconnecting");
-        });
-        Vital::Tool::Event::bind("network:server_disconnected", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Lost connection to server");
-        });
-        Vital::Tool::Event::bind("network:disconnected", [](Vital::Tool::Stack&) {
-            Vital::print("sbox", "Disconnected cleanly");
-        });
-        net->set_reconnect_config(5, 3.0f);
-    }
+    #if !defined(Vital_SDK_Client)
+    Vital::Tool::Event::bind("network:hosted", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Server is live");
+    });
+    Vital::Tool::Event::bind("network:peer_joined", [](Vital::Tool::Stack& args) {
+        int32_t id = args.array[0].as<int32_t>();
+        Vital::print("sbox", "Player joined: ", id);
+    });
+    Vital::Tool::Event::bind("network:peer_left", [](Vital::Tool::Stack& args) {
+        int32_t id = args.array[0].as<int32_t>();
+        Vital::print("sbox", "Player left: ", id);
+    });
+    Vital::Tool::Event::bind("network:packet", [net](Vital::Tool::Stack& args) {
+        int32_t sender = args.object.at("sender_id").as<int32_t>();
+        std::string msg = args.array[0].as<std::string>();
+        Vital::print("sbox", "Msg from ", sender, ": ", msg.c_str());
+        Vital::Tool::Stack reply;
+        reply.array.push_back(Vital::Tool::StackValue(std::string("pong")));
+        net->send(reply, sender);
+    });
+    Vital::Tool::Event::bind("network:closed", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Server closed");
+    });
+    #endif
+
+    #if defined(Vital_SDK_Client)
+    Vital::Tool::Event::bind("network:connecting", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Connecting...");
+    });
+    Vital::Tool::Event::bind("network:connected", [net](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Connected! My ID: ", net->get_peer_id());
+        Vital::Tool::Stack msg;
+        msg.array.push_back(Vital::Tool::StackValue(std::string("ping")));
+        net->send_to_server(msg);
+    });
+    Vital::Tool::Event::bind("network:packet", [](Vital::Tool::Stack& args) {
+        std::string msg = args.array[0].as<std::string>();
+        Vital::print("sbox", "Server replied: ", msg.c_str());
+    });
+    Vital::Tool::Event::bind("network:connection_failed", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Failed to connect");
+    });
+    Vital::Tool::Event::bind("network:reconnecting", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Retrying...");
+    });
+    Vital::Tool::Event::bind("network:reconnect_failed", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Gave up reconnecting");
+    });
+    Vital::Tool::Event::bind("network:server_disconnected", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Lost connection to server");
+    });
+    Vital::Tool::Event::bind("network:disconnected", [](Vital::Tool::Stack&) {
+        Vital::print("sbox", "Disconnected cleanly");
+    });
+    net->set_reconnect_config(5, 3.0f);
+    #endif
 }
 
 void initialize_vital_events() {
