c12f28a6b262aedab2a92ee957c56fde5ccf1d89
A <89867985+ov-aviril@users.noreply.github.com>
2026-03-31 14:51:57 +0300

    Update .gitreload.sh

--- .gitreload.sh
+++ .gitreload.sh
@@ -1,3 +1,8 @@
+if [ "$CI" = "true" ]; then
+    echo "CI detected — skipping vendor reload"
+    exit 0
+fi
+
 git config --global submodule.recurse true
 git submodule sync
 git submodule update --init --recursive --remote
