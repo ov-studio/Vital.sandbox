25097c4cdb221bade6b16956d6c2ebb865ea6591
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-03 16:15:24 +0300

    Deleted vcpkg - not used anymore
    Co-Authored-By: ov - Tron <48655846+ov-tron@users.noreply.github.com>

--- Vital.sandbox/Bootstrap/module.py
+++ Vital.sandbox/Bootstrap/module.py
@@ -1,7 +1,6 @@
 from Bootstrap.utils import *
 from Bootstrap.vendor import *
 from Bootstrap.conan import *
-from Bootstrap.vcpkg import *
 
 class Module:
     def __init__(self, env):
@@ -10,14 +9,12 @@ class Module:
     def build(self):
         self.env.Vendor.build()
         self.env.Conan.build()
-        self.env.VCPKG.build()
         self.env.Discord.build()
 
     def stage(self, build):
         cwd = os.path.abspath(os.getcwd())
         vital_dir = os.path.normpath(os.path.join(cwd, "..", f"Vital.{self.env.Args['platform_type'].lower()}", self.env.name.lower(), self.env['platform']))
         build_dir = os.path.dirname(str(build[0].abspath))
-        self.env.VCPKG.stage(build, build_dir)
         self.env.Discord.stage(build, build_dir)
         vital_stage = self.env.Alias("stage", self.env.Install(vital_dir, self.env.Glob(os.path.join(build_dir, "*"))))
         self.env.Depends(vital_stage, build)
