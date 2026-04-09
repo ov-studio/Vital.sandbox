8d4581441fcc4640b2912261d174378f505a6cb7
A <48655846+ov-tron@users.noreply.github.com>
2026-04-09 19:06:56 +0300

    Fix > cl : Command line warning D9025 : overriding

--- build.py
+++ build.py
@@ -78,8 +78,10 @@ class Build:
             "scons", "-C", b["sandbox_dir"],
             f"platform_type={self.platform_type}",
             f"build_type={self.build_type}",
-            "build_library=no",
             f"target=template_{self.build_type.lower()}",
+            "use_static_cpp=no",
+            "use_static_crt=no",
+            "build_library=no",
             f"-j{int(self.os_info['nproc'])}",
         ]
 
