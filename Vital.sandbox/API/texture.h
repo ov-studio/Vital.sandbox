7f565e4541dbf773a942d2a45f0ed9ea2bb77da0
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-14 22:05:04 +0300

    Update texture.h

--- Vital.sandbox/API/texture.h
+++ Vital.sandbox/API/texture.h
@@ -31,8 +31,10 @@ namespace Vital::Sandbox::API {
         static void bind(Machine* vm) {
             vm_module::register_type<Texture>(vm, base_name);
 
-            API::bind(vm, {base_name}, "create", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+            API::bind(vm, {base_name}, "create", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(path)")
+                    .require(1, &Machine::is_string);
+
                 auto path = vm -> get_string(1);
                 auto object = base_class::create_texture_2d(path);
                 vm -> create_object(base_name, object);
@@ -41,7 +43,7 @@ namespace Vital::Sandbox::API {
         }
 
         static void methods(Machine* vm) {
-            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self) -> int {
+            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self, auto& id) -> int {
                 self -> destroy();
                 vm_module::release_userdata(vm, 1);
                 vm -> push_value(true);
@@ -57,16 +59,20 @@ namespace Vital::Sandbox::API {
         static void bind(Machine* vm) {
             vm_module::register_type<SVG>(vm, base_name);
 
-            API::bind(vm, {base_name}, "create", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+            API::bind(vm, {base_name}, "create", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(path)")
+                    .require(1, &Machine::is_string);
+
                 auto path = vm -> get_string(1);
                 auto object = base_class::create_svg(path);
                 vm -> create_object(base_name, object);
                 return 1;
             });
 
-            API::bind(vm, {base_name}, "create_from_raw", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_string(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+            API::bind(vm, {base_name}, "create_from_raw", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(raw)")
+                    .require(1, &Machine::is_string);
+
                 auto raw = vm -> get_string(1);
                 auto object = base_class::create_svg_from_raw(raw);
                 vm -> create_object(base_name, object);
@@ -75,15 +81,17 @@ namespace Vital::Sandbox::API {
         }
 
         static void methods(Machine* vm) {
-            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self) -> int {
+            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self, auto& id) -> int {
                 self -> destroy();
                 vm_module::release_userdata(vm, 1);
                 vm -> push_value(true);
                 return 1;
             });
 
-            vm_module::bind_method<base_class>(vm, base_name, "update", [](auto vm, auto self) -> int {
-                if ((vm -> get_count() < 2) || (!vm -> is_string(2))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+            vm_module::bind_method<base_class>(vm, base_name, "update", [](auto vm, auto self, auto& id) -> int {
+                vm_args(vm, id, "(raw)")
+                    .require(2, &Machine::is_string);
+
                 auto raw = vm -> get_string(2);
                 self -> update_svg_from_raw(raw);
                 vm -> push_value(true);
