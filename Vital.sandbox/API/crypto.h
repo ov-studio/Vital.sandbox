93252b3fecf0ce76c7b08b03861e4a77eeb1d449
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-14 21:42:59 +0300

    Update crypto.h

--- Vital.sandbox/API/crypto.h
+++ Vital.sandbox/API/crypto.h
@@ -25,41 +25,41 @@ namespace Vital::Sandbox::API {
         inline static const std::string base_name = "crypto";
 
         static void bind(Machine* vm) {
-            API::bind(vm, {base_name}, "hash", [](auto vm) -> int {
-                vm_args(vm, "crypto.hash(mode, input)")
+            API::bind(vm, {base_name}, "hash", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(mode, input)")
                     .require(1, &Machine::is_string)
                     .require(2, &Machine::is_string);
-                    
+        
                 auto mode = vm -> get_string(1);
                 auto input = vm -> get_string(2);
                 vm -> push_value(Tool::Crypto::hash(mode, input));
                 return 1;
             });
-
-            API::bind(vm, {base_name}, "encode", [](auto vm) -> int {
-                vm_args(vm, "crypto.encode(input)")
+        
+            API::bind(vm, {base_name}, "encode", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(input)")
                     .require(1, &Machine::is_string);
-
+        
                 auto input = vm -> get_string(1);
                 vm -> push_value(Tool::Crypto::encode(input));
                 return 1;
             });
-
-            API::bind(vm, {base_name}, "decode", [](auto vm) -> int {
-                vm_args(vm, "crypto.decode(input)")
+        
+            API::bind(vm, {base_name}, "decode", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(input)")
                     .require(1, &Machine::is_string);
-
+        
                 auto input = vm -> get_string(1);
                 vm -> push_value(Tool::Crypto::decode(input));
                 return 1;
             });
-
-            API::bind(vm, {base_name}, "encrypt", [](auto vm) -> int {
-                vm_args(vm, "crypto.encrypt(mode, input, key)")
+        
+            API::bind(vm, {base_name}, "encrypt", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(mode, input, key)")
                     .require(1, &Machine::is_string)
                     .require(2, &Machine::is_string)
                     .require(3, &Machine::is_string);
-
+        
                 auto mode = vm -> get_string(1);
                 auto input = vm -> get_string(2);
                 auto key = vm -> get_string(3);
@@ -68,14 +68,14 @@ namespace Vital::Sandbox::API {
                 vm -> push_value(result.second);
                 return 2;
             });
-
-            API::bind(vm, {base_name}, "decrypt", [](auto vm) -> int {
-                vm_args(vm, "crypto.decrypt(mode, input, key, iv)")
+        
+            API::bind(vm, {base_name}, "decrypt", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(mode, input, key, iv)")
                     .require(1, &Machine::is_string)
                     .require(2, &Machine::is_string)
                     .require(3, &Machine::is_string)
                     .require(4, &Machine::is_string);
-
+        
                 auto mode = vm -> get_string(1);
                 auto input = vm -> get_string(2);
                 auto key = vm -> get_string(3);
