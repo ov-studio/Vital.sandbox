ddde7d186f70d57ceaa762d7721f2f4ffc765da2
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-15 08:48:19 +0300

    Update ssr.h

--- Vital.sandbox/API/gfx/ssr.h
+++ Vital.sandbox/API/gfx/ssr.h
@@ -21,74 +21,83 @@
 // Vital: API: SSR //
 //////////////////////
 
-// TODO: Update API
 namespace Vital::Sandbox::API {
     struct SSR : vm_module {
         inline static const std::string base_name = "gfx";
         using base_class = Vital::Engine::Core;
 
         static void bind(Machine* vm) {
-            API::bind(vm, {base_name, "ssr"}, "is_enabled", [](auto vm) -> int {
+            API::bind(vm, {base_name, "ssr"}, "is_enabled", [](auto vm, auto& id) -> int {
                 vm -> push_value(base_class::get_environment() -> is_ssr_enabled());
                 return 1;
             });
 
-            API::bind(vm, {base_name, "ssr"}, "set_enabled", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_bool(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+            API::bind(vm, {base_name, "ssr"}, "set_enabled", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(state)")
+                    .require(1, &Machine::is_bool);
+
                 auto state = vm -> get_bool(1);
                 base_class::get_environment() -> set_ssr_enabled(state);
                 vm -> push_value(true);
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "set_max_steps", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_number(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+
+            API::bind(vm, {base_name, "ssr"}, "set_max_steps", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(value)")
+                    .require(1, &Machine::is_number);
+
                 auto value = vm -> get_int(1);
                 base_class::get_environment() -> set_ssr_max_steps(value);
                 vm -> push_value(true);
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "get_max_steps", [](auto vm) -> int {
+
+            API::bind(vm, {base_name, "ssr"}, "get_max_steps", [](auto vm, auto& id) -> int {
                 vm -> push_value(base_class::get_environment() -> get_ssr_max_steps());
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "set_fade_in", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_number(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+
+            API::bind(vm, {base_name, "ssr"}, "set_fade_in", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(value)")
+                    .require(1, &Machine::is_number);
+
                 auto value = vm -> get_float(1);
                 base_class::get_environment() -> set_ssr_fade_in(value);
                 vm -> push_value(true);
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "get_fade_in", [](auto vm) -> int {
+
+            API::bind(vm, {base_name, "ssr"}, "get_fade_in", [](auto vm, auto& id) -> int {
                 vm -> push_value(base_class::get_environment() -> get_ssr_fade_in());
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "set_fade_out", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_number(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+
+            API::bind(vm, {base_name, "ssr"}, "set_fade_out", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(value)")
+                    .require(1, &Machine::is_number);
+
                 auto value = vm -> get_float(1);
                 base_class::get_environment() -> set_ssr_fade_out(value);
                 vm -> push_value(true);
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "get_fade_out", [](auto vm) -> int {
+
+            API::bind(vm, {base_name, "ssr"}, "get_fade_out", [](auto vm, auto& id) -> int {
                 vm -> push_value(base_class::get_environment() -> get_ssr_fade_out());
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "set_depth_tolerance", [](auto vm) -> int {
-                if ((vm -> get_count() < 1) || (!vm -> is_number(1))) throw Vital::Log::fetch("invalid-arguments", Vital::Log::Type::Error);
+
+            API::bind(vm, {base_name, "ssr"}, "set_depth_tolerance", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(value)")
+                    .require(1, &Machine::is_number);
+
                 auto value = vm -> get_float(1);
                 base_class::get_environment() -> set_ssr_depth_tolerance(value);
                 vm -> push_value(true);
                 return 1;
             });
-        
-            API::bind(vm, {base_name, "ssr"}, "get_depth_tolerance", [](auto vm) -> int {
+
+            API::bind(vm, {base_name, "ssr"}, "get_depth_tolerance", [](auto vm, auto& id) -> int {
                 vm -> push_value(base_class::get_environment() -> get_ssr_depth_tolerance());
                 return 1;
             });
