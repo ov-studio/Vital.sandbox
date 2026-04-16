6927ee375c871f2329e6dcc73f1bdc9bd64acd89
A <89867985+ov-aviril@users.noreply.github.com>
2026-04-17 00:19:31 +0300

    Update resource.cpp

--- Vital.sandbox/Manager/private/resource.cpp
+++ Vital.sandbox/Manager/private/resource.cpp
@@ -194,11 +194,9 @@ namespace Vital::Manager {
             Tool::Event::bind("vital.network:packet", [this](Tool::Stack args) {
                 if (!args.object.count("event")) return;
                 const std::string event = args.object.at("event").as<std::string>();
-                if (event.rfind("vital.resource:", 0) != 0) return;
-
-                auto rm = Resource::get_singleton();
                 if (event == "vital.resource:started") {
                     if (!args.object.count("name")) return;
+                    auto rm = Resource::get_singleton();
                     const std::string name = args.object.at("name").as<std::string>();
                     std::vector<Script> scripts;
                     std::vector<std::string> files;
@@ -208,6 +206,7 @@ namespace Vital::Manager {
                 }
                 else if (event == "vital.resource:stopped") {
                     if (!args.object.count("name")) return;
+                    auto rm = Resource::get_singleton();
                     const std::string name = args.object.at("name").as<std::string>();
                     log("sbox", fmt::format("client received resource stop: `{}`", name));
                     Resource::get_singleton() -> unload(name);
