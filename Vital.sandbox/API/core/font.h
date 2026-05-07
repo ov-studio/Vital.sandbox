ef11d830b8f75ab4e435cf972d04820ec4e01b2d
A <103286142+ov-anisa@users.noreply.github.com>
2026-05-07 11:09:42 +0300

    Adapted font module to `vm_instance`

--- Vital.sandbox/API/core/font.h
+++ Vital.sandbox/API/core/font.h
@@ -27,6 +27,23 @@ namespace Vital::Sandbox::API {
         inline static const std::string base_name = "font";
         using base_class = Vital::Engine::Font;
 
+        struct Instance : vm_instance<Instance> {
+            using Owner = Font;
+            base_class* font = nullptr;
+        };
+        inline static std::mutex mutex;
+        inline static std::unordered_map<int, std::shared_ptr<Instance>> buffer;
+        inline static std::atomic<int> next_id { 1 };
+
+        static void clean_instance(std::shared_ptr<Instance> instance) {
+            if (!Instance::erase(instance)) return;
+            if (instance -> font) {
+                instance -> font -> destroy();
+                instance -> font = nullptr;
+            }
+            Instance::release(instance);
+        }
+
         static void bind(Machine* vm) {
             vm_module::register_type<Font>(vm, base_name);
 
@@ -35,47 +52,51 @@ namespace Vital::Sandbox::API {
                     .require(1, &Machine::is_string);
 
                 auto path = vm -> get_string(1);
-                auto object = base_class::create(path);
-                vm -> create_object(base_name, object);
+                auto instance = Instance::init(vm);
+                instance -> font = base_class::create(path);
+                Instance::store(instance);
+                vm -> create_object(base_name, instance.get());
+                instance -> userdata = vm_module::get_userdata_ptr(vm, -1);
                 return 1;
             });
         }
 
         static void methods(Machine* vm) {
-            vm_module::bind_method<base_class>(vm, base_name, "destroy", [](auto vm, auto self, auto& id) -> int {
-                self -> destroy();
-                vm_module::release_userdata(vm, 1);
-                vm -> push_value(true);
-                return 1;
+            vm_module::bind_method<Instance>(vm, base_name, "destroy", [](auto vm, auto self, auto& id) -> int {
+                return Instance::destroy(vm);
             });
 
-            vm_module::bind_method<base_class>(vm, base_name, "set_antialiasing", [](auto vm, auto self, auto& id) -> int {
+            vm_module::bind_method<Instance>(vm, base_name, "set_antialiasing", [](auto vm, auto self, auto& id) -> int {
                 vm_args(vm, id, "(state)")
                     .require(2, &Machine::is_bool);
 
-                self -> set_antialiasing(vm -> get_bool(2));
+                self -> font -> set_antialiasing(vm -> get_bool(2));
                 vm -> push_value(true);
                 return 1;
             });
 
-            vm_module::bind_method<base_class>(vm, base_name, "get_antialiasing", [](auto vm, auto self, auto& id) -> int {
-                vm -> push_value(self -> get_antialiasing());
+            vm_module::bind_method<Instance>(vm, base_name, "get_antialiasing", [](auto vm, auto self, auto& id) -> int {
+                vm -> push_value(self -> font -> get_antialiasing());
                 return 1;
             });
 
-            vm_module::bind_method<base_class>(vm, base_name, "set_oversampling", [](auto vm, auto self, auto& id) -> int {
+            vm_module::bind_method<Instance>(vm, base_name, "set_oversampling", [](auto vm, auto self, auto& id) -> int {
                 vm_args(vm, id, "(value)")
                     .require(2, &Machine::is_number);
 
-                self -> set_oversampling(static_cast<float>(vm -> get_float(2)));
+                self -> font -> set_oversampling(static_cast<float>(vm -> get_float(2)));
                 vm -> push_value(true);
                 return 1;
             });
 
-            vm_module::bind_method<base_class>(vm, base_name, "get_oversampling", [](auto vm, auto self, auto& id) -> int {
-                vm -> push_value(self -> get_oversampling());
+            vm_module::bind_method<Instance>(vm, base_name, "get_oversampling", [](auto vm, auto self, auto& id) -> int {
+                vm -> push_value(self -> font -> get_oversampling());
                 return 1;
             });
+        }
+
+        static void clean(const std::string& env) {
+            vm_module::collect_env<Instance>(mutex, buffer, env, clean_instance);
         }
     };
 }
