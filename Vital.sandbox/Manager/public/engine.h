dcf8dda803bfe251b0198034060bc9722d3f0436
A <103286142+ov-anisa@users.noreply.github.com>
2026-05-08 17:43:15 +0300

    Update engine.h

--- Vital.sandbox/Manager/public/engine.h
+++ Vital.sandbox/Manager/public/engine.h
@@ -52,7 +52,7 @@ inline void uninitialize_gdextension_types(godot::ModuleInitializationLevel p_le
 }
 
 extern "C" {
-    inline GDExtensionBool GDE_EXPORT vital_sandbox_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization) {
+    inline GDExtensionBool GDE_EXPORT vsdk_entrypoint(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization) {
         godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
         init_obj.register_initializer(initialize_gdextension_types);
         init_obj.register_terminator(uninitialize_gdextension_types);
