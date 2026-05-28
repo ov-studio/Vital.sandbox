5d814cd3aae6fcebeede6609222a124b95758350
A <89867985+ov-aviril@users.noreply.github.com>
2026-05-28 12:23:18 +0300

    Update webview.h

--- Vital.sandbox/API/core/webview.h
+++ Vital.sandbox/API/core/webview.h
@@ -61,9 +61,7 @@ namespace Vital::Sandbox::API {
                 auto instance = Instance::init(vm);
                 instance -> webview = base_class::create(options);
                 Instance::store(instance);
-                vm -> create_object(base_name, instance.get());
-                instance -> userdata = vm_module::get_userdata_ptr(vm, -1);
-                instance -> set_ref(instance -> self_reference(), -1);
+                Instance::bind(vm, base_name, instance);
                 return 1;
             });
         }
