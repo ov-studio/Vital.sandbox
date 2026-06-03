b8afca239d19d4dfbfc40727c61c5fcd5f638e1f
A <crazynuggets@outlook.com>
2026-06-03 09:51:12 +0300

    Separated apis into its own header

--- Vital.sandbox/Sandbox/index.cpp
+++ Vital.sandbox/Sandbox/index.cpp
@@ -16,37 +16,7 @@
 #include <Vital.sandbox/Sandbox/machine.h>
 #include <Vital.sandbox/Engine/public/core.h>
 #include <Vital.sandbox/Engine/public/console.h>
-
-#include <Vital.sandbox/API/core/engine.h>
-#include <Vital.sandbox/API/core/canvas.h>
-#include <Vital.sandbox/API/core/database.h>
-#include <Vital.sandbox/API/core/database_query.h>
-#include <Vital.sandbox/API/core/discord.h>
-#include <Vital.sandbox/API/core/network.h>
-#include <Vital.sandbox/API/core/model.h>
-#include <Vital.sandbox/API/core/webview.h>
-#include <Vital.sandbox/API/core/font.h>
-#include <Vital.sandbox/API/core/texture.h>
-#include <Vital.sandbox/API/core/svg.h>
-#include <Vital.sandbox/API/core/rendertarget.h>
-
-#include <Vital.sandbox/API/utility/timer.h>
-#include <Vital.sandbox/API/utility/promise.h>
-#include <Vital.sandbox/API/utility/thread.h>
-#include <Vital.sandbox/API/utility/file.h>
-#include <Vital.sandbox/API/utility/http.h>
-#include <Vital.sandbox/API/utility/crypto.h>
-#include <Vital.sandbox/API/utility/shrinker.h>
-#include <Vital.sandbox/API/utility/exports.h>
-
-#include <Vital.sandbox/API/gfx/adjustment.h>
-#include <Vital.sandbox/API/gfx/emissive.h>
-#include <Vital.sandbox/API/gfx/ssr.h>
-#include <Vital.sandbox/API/gfx/ssil.h>
-#include <Vital.sandbox/API/gfx/sdfgi.h>
-#include <Vital.sandbox/API/gfx/ssao.h>
-#include <Vital.sandbox/API/gfx/fog.h>
-#include <Vital.sandbox/API/gfx/volumetric_fog.h>
+#include <Vital.sandbox/Sandbox/runtime/apis.h>
 
 
 /////////////////////
---
+++ Vital.sandbox/Sandbox/runtime/apis.h
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Sandbox: apis.h
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Sandbox APIs
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.sandbox/API/core/engine.h>
+#include <Vital.sandbox/API/core/canvas.h>
+#include <Vital.sandbox/API/core/database.h>
+#include <Vital.sandbox/API/core/database_query.h>
+#include <Vital.sandbox/API/core/discord.h>
+#include <Vital.sandbox/API/core/network.h>
+#include <Vital.sandbox/API/core/model.h>
+#include <Vital.sandbox/API/core/webview.h>
+#include <Vital.sandbox/API/core/font.h>
+#include <Vital.sandbox/API/core/texture.h>
+#include <Vital.sandbox/API/core/svg.h>
+#include <Vital.sandbox/API/core/rendertarget.h>
+
+#include <Vital.sandbox/API/utility/timer.h>
+#include <Vital.sandbox/API/utility/promise.h>
+#include <Vital.sandbox/API/utility/thread.h>
+#include <Vital.sandbox/API/utility/file.h>
+#include <Vital.sandbox/API/utility/http.h>
+#include <Vital.sandbox/API/utility/crypto.h>
+#include <Vital.sandbox/API/utility/shrinker.h>
+#include <Vital.sandbox/API/utility/exports.h>
+
+#include <Vital.sandbox/API/gfx/adjustment.h>
+#include <Vital.sandbox/API/gfx/emissive.h>
+#include <Vital.sandbox/API/gfx/ssr.h>
+#include <Vital.sandbox/API/gfx/ssil.h>
+#include <Vital.sandbox/API/gfx/sdfgi.h>
+#include <Vital.sandbox/API/gfx/ssao.h>
+#include <Vital.sandbox/API/gfx/fog.h>
+#include <Vital.sandbox/API/gfx/volumetric_fog.h>
