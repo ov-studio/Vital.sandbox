3fbc08715f67a975aa81d77a1d25d3ca706ca37a
A <crazynuggets@outlook.com>
2026-06-11 20:30:01 +0300

    Exposed few resource apis to lua

--- Vital.sandbox/API/utility/resource.h
+++ Vital.sandbox/API/utility/resource.h
@@ -32,6 +32,59 @@ namespace Vital::Sandbox::API {
                 else vm -> push_value(name);
                 return 1;
             });
+        
+            API::bind(vm, {base_name}, "is_loaded", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(name)")
+                    .require(1, &Machine::is_string);
+
+                vm -> push_value(Manager::Resource::get_singleton() -> is_loaded(vm -> get_string(1)));
+                return 1;
+            });
+        
+            API::bind(vm, {base_name}, "is_running", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(name)")
+                    .require(1, &Machine::is_string);
+
+                vm -> push_value(Manager::Resource::get_singleton() -> is_running(vm -> get_string(1)));
+                return 1;
+            });
+        
+            API::bind(vm, {base_name}, "get_list", [](auto vm, auto& id) -> int {
+                auto resources = Manager::Resource::get_singleton() -> get_all_resources();
+                vm -> create_table();
+                int i = 1;
+                for (const auto& manifest : resources) {
+                    vm -> push_value(manifest -> ref);
+                    vm -> set_table_field(i++, -2);
+                }
+                return 1;
+            });
+        
+            #if !defined(VSDK_Client)
+            API::bind(vm, {base_name}, "start", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(name)")
+                    .require(1, &Machine::is_string);
+
+                vm -> push_value(Manager::Resource::get_singleton() -> start(vm -> get_string(1)));
+                return 1;
+            });
+        
+            API::bind(vm, {base_name}, "stop", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(name)")
+                    .require(1, &Machine::is_string);
+
+                vm -> push_value(Manager::Resource::get_singleton() -> stop(vm -> get_string(1)));
+                return 1;
+            });
+        
+            API::bind(vm, {base_name}, "restart", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(name)")
+                    .require(1, &Machine::is_string);
+
+                vm -> push_value(Manager::Resource::get_singleton() -> restart(vm -> get_string(1)));
+                return 1;
+            });
+            #endif
         }
     };
 }
