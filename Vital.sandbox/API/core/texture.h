b4406ec5831bb2ae675e635463753700bad4e824
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-12 09:58:43 +0300

    Update texture.h

--- Vital.sandbox/API/core/texture.h
+++ Vital.sandbox/API/core/texture.h
@@ -63,9 +63,7 @@ namespace Vital::Sandbox::API {
 
             vm -> create_namespace(base_name);
             vm -> create_table();
-            for (const auto& [name, format] : base_class::texel_registry) {
-                vm -> table_set_value(name, format);
-            }
+            for (const auto& [name, format] : base_class::texel_registry) vm -> table_set_value(name, format);
             vm -> set_table_field("texel_format", -2);
             vm -> pop(1);
         }
