171e419098f7fd2e6380b0a17e11ea9d4ac5a49d
A <103286142+ov-anisa@users.noreply.github.com>
2026-06-14 15:14:40 +0300

    Update conanfile.py

--- Vital.sandbox/conanfile.py
+++ Vital.sandbox/conanfile.py
@@ -7,10 +7,20 @@ class BuildConan(ConanFile):
     generators = "SConsDeps"
     
     def requirements(self):
+        self.requires("openssl/3.5.1")
+        self.requires("cpp-httplib/0.39.0")
         self.requires("libmysqlclient/8.1.0")
         self.requires("soci/4.1.2")
+        self.requires("zstd/1.5.7")
+        self.requires("libzip/1.11.4")
+        self.requires("rapidjson/1.1.0")
+        self.requires("rapidyaml/0.10.0")
+        self.requires("fmt/12.1.0")
 
     def configure(self):
+        os_info = Fetch_OS()
+        self.options["openssl"].shared = False
+        self.options["cpp-httplib"].shared = False
         self.options["libmysqlclient"].shared = False
         self.options["soci"].shared = False
         self.options["soci"].with_mysql = True
@@ -18,3 +28,8 @@ class BuildConan(ConanFile):
         self.options["soci"].with_postgresql = False
         self.options["soci"].with_odbc = False
         self.options["soci"].with_boost = False
+        self.options["zstd"].shared = False
+        self.options["libzip"].shared = False
+        self.options["rapidjson"].shared = False
+        self.options["rapidyaml"].shared = False
+        self.options["fmt"].shared = False
