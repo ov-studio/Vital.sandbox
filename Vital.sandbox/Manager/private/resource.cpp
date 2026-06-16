806ba87b91bbfbb2722f346186149bbb1d4d637f
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-16 09:19:55 +0300

    Update resource.cpp

--- Vital.sandbox/Manager/private/resource.cpp
+++ Vital.sandbox/Manager/private/resource.cpp
@@ -589,14 +589,14 @@ namespace Vital::Manager {
         #if defined(VSDK_Client)
             Tool::Event::bind("asset:group_ready", [this](Tool::Stack arguments) {
                 if (!arguments.object.count("group")) return;
-                
+
                 const std::string name = arguments.object.at("group").as<std::string>();
-                log("sbox", fmt::format("resource `{}` all assets ready — executing scripts", name));
                 {
                     auto rm = Resource::get_singleton();
                     std::lock_guard<std::mutex> lock(rm -> mutex);
                     if (!Internal::is_loaded(name)) return;
                 }
+                log("sbox", fmt::format("resource `{}` all assets ready — executing scripts", name));
                 Engine::Core::get_singleton() -> enqueue([name]() { Internal::start(name); });
             });
 
