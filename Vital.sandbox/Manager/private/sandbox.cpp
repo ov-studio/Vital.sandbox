dc1d02c8bd1ee7cb5abe1eaea6be8eb2fd4fe136
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-28 20:56:52 +0300

    Update sandbox.cpp

--- Vital.sandbox/Manager/private/sandbox.cpp
+++ Vital.sandbox/Manager/private/sandbox.cpp
@@ -16,6 +16,7 @@
 #include <Vital.sandbox/Manager/public/sandbox.h>
 #include <Vital.sandbox/Engine/public/core.h>
 #include <Vital.sandbox/Engine/public/canvas.h>
+#include <Vital.sandbox/Engine/public/console.h>
 
 
 /////////////////////////////
