083be96435a0fe2143f9df18a62d49463d754074
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-02 14:25:49 +0300

    Added precompiled manager header

---
+++ Vital.sandbox/Vital/manager.hpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Vital: manager.hpp
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Precompiled Manager Header
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.sandbox/Manager/public/kit.h>
+#include <Vital.sandbox/Manager/public/asset.h>
+#include <Vital.sandbox/Manager/public/network.h>
+#include <Vital.sandbox/Manager/public/sandbox.h>
+#include <Vital.sandbox/Manager/public/discord.h>
+#include <Vital.sandbox/Manager/public/resource.h> 
