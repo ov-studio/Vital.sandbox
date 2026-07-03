505d32b26b32bfd11b76f2a06fac803052cab714
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-03 12:12:02 +0300

    Update tool.h

--- Vital.sandbox/Vital/tool.h
+++ Vital.sandbox/Vital/tool.h
@@ -13,4 +13,18 @@
 //////////////
 
 #pragma once
-#include <Vital.sandbox/Sandbox/machine.h>
+#include <Vital.sandbox/Tool/index.h>
+#include <Vital.sandbox/Tool/log.h>
+#include <Vital.sandbox/Tool/yaml.h>
+#include <Vital.sandbox/Tool/stack.h>
+#include <Vital.sandbox/Tool/thread.h>
+#include <Vital.sandbox/Tool/timer.h>
+#include <Vital.sandbox/Tool/file.h>
+#include <Vital.sandbox/Tool/format.h>
+#include <Vital.sandbox/Tool/event.h>
+#include <Vital.sandbox/Tool/database.h>
+#include <Vital.sandbox/Tool/http.h>
+#include <Vital.sandbox/Tool/inspect.h>
+#include <Vital.sandbox/Tool/crypto.h>
+#include <Vital.sandbox/Tool/shrinker.h>
+#include <Vital.sandbox/Tool/base.h>
