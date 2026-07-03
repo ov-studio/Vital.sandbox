1c4ada7d7a3810dfa53d6ebd920b639db6a7a570
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-03 12:12:19 +0300

    Create sandbox.h

---
+++ Vital.sandbox/Vital/sandbox.h
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Vital: sandbox.h
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Precompiled Sandbox Header
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.sandbox/Sandbox/machine.h>
