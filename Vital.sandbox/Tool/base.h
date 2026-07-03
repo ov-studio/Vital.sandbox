7cecec8027fd468ce316a0d60ad1e4c28e9f7e74
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-03 12:13:24 +0300

    Init base class tools

---
+++ Vital.sandbox/Tool/base.h
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Tool: base.h
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Base Tools
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#include <Vital.sandbox/Tool/log.h>
+
+
+////////////////////////
+// Vital: Tool: Base //
+////////////////////////
+
+namespace Vital::Tool {
+    // Derived must declare:
+    // static constexpr const char* Name = "ClassName";
+    // friend class Base<Derived>; (constructor/destructor stay private)
+    
+    template<typename Derived>
+    class Base {
+        protected:
+            inline static Derived* singleton = nullptr;
+            Base() = default;
+            virtual ~Base() = default;
+        public:
+            // Singleton //
+            static Derived* get_singleton() {
+                if (!singleton) singleton = new Derived();
+                return singleton;
+            }
+
+            static void free_singleton() {
+                if (!singleton) return;
+                delete singleton;
+                singleton = nullptr;
+            }
+
+
+            // Managers //
+            inline void log(const std::string& mode, const std::string& message) const {
+                Tool::print(mode, fmt::format("{}.manager: {}", Derived::Name, message));
+            }
+    };
+}
