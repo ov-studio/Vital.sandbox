6dedba599f3c3c56088ede349066f1d3104bfa84
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-07 12:55:58 +0300

    Validate enum using `validate_enum`

--- Vital.sandbox/API/gfx/adjustment.h
+++ Vital.sandbox/API/gfx/adjustment.h
@@ -158,7 +158,7 @@ namespace Vital::Sandbox::API {
                 vm -> push_value(true);
                 return 1;
             });
-
+    
             API::bind(vm, base_scope, "get_tonemapper", [](auto vm, auto& id) -> int {
                 vm -> push_value(static_cast<int>(base_class::get_environment() -> get_tonemapper()));
                 return 1;
@@ -166,7 +166,8 @@ namespace Vital::Sandbox::API {
 
             API::bind(vm, base_scope, "set_tonemapper", [](auto vm, auto& id) -> int {
                 vm_args(vm, id, "(mode)")
-                    .require(1, &Machine::is_number);
+                    .require(1, &Machine::is_number)
+                    .validate_enum(1, godot::Environment::TONE_MAPPER_LINEAR, godot::Environment::TONE_MAPPER_ACES);
 
                 auto mode = static_cast<godot::Environment::ToneMapper>(vm -> get_int(1));
                 base_class::get_environment() -> set_tonemapper(mode);
@@ -200,6 +201,177 @@ namespace Vital::Sandbox::API {
 
                 auto value = vm -> get_float(1);
                 base_class::get_environment() -> set_tonemap_white(value);
+                vm -> push_value(true);
+                return 1;
+            });
+        
+
+            // Background / Sky / Ambient / Tonemap //
+
+            API::bind(vm, base_scope, "get_background", [](auto vm, auto& id) -> int {
+                vm -> push_value(static_cast<int>(base_class::get_environment() -> get_background()));
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_background", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(mode)")
+                    .require(1, &Machine::is_number)
+                    .validate_enum(1, godot::Environment::BG_CLEAR_COLOR, godot::Environment::BG_CAMERA_FEED);
+
+                auto mode = static_cast<godot::Environment::BGMode>(vm -> get_int(1));
+                base_class::get_environment() -> set_background(mode);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_sky_rotation", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_sky_rotation());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_sky_rotation", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(euler_radians)")
+                    .require(1, &Machine::is_vector3);
+
+                auto value = vm -> get_vector3(1);
+                base_class::get_environment() -> set_sky_rotation(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_bg_color", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_bg_color());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_bg_color", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(color)")
+                    .require(1, &Machine::is_color);
+
+                auto value = vm -> get_color(1);
+                base_class::get_environment() -> set_bg_color(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_bg_intensity", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_bg_intensity());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_bg_intensity", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(energy)")
+                    .require(1, &Machine::is_number);
+
+                auto value = vm -> get_float(1);
+                base_class::get_environment() -> set_bg_intensity(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_canvas_max_layer", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_canvas_max_layer());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_canvas_max_layer", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(layer)")
+                    .require(1, &Machine::is_number);
+
+                auto value = vm -> get_int(1);
+                base_class::get_environment() -> set_canvas_max_layer(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_camera_feed_id", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_camera_feed_id());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_camera_feed_id", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(id)")
+                    .require(1, &Machine::is_number);
+
+                auto value = vm -> get_int(1);
+                base_class::get_environment() -> set_camera_feed_id(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_ambient_color", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_ambient_light_color());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_ambient_color", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(color)")
+                    .require(1, &Machine::is_color);
+
+                auto value = vm -> get_color(1);
+                base_class::get_environment() -> set_ambient_light_color(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_ambient_source", [](auto vm, auto& id) -> int {
+                vm -> push_value(static_cast<int>(base_class::get_environment() -> get_ambient_source()));
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_ambient_source", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(source)")
+                    .require(1, &Machine::is_number)
+                    .validate_enum(1, godot::Environment::AMBIENT_SOURCE_BG, godot::Environment::AMBIENT_SOURCE_SKY);
+
+                auto source = static_cast<godot::Environment::AmbientSource>(vm -> get_int(1));
+                base_class::get_environment() -> set_ambient_source(source);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_ambient_energy", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_ambient_light_energy());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_ambient_energy", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(energy)")
+                    .require(1, &Machine::is_number);
+
+                auto value = vm -> get_float(1);
+                base_class::get_environment() -> set_ambient_light_energy(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_ambient_sky_contribution", [](auto vm, auto& id) -> int {
+                vm -> push_value(base_class::get_environment() -> get_ambient_light_sky_contribution());
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_ambient_sky_contribution", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(ratio)")
+                    .require(1, &Machine::is_number);
+
+                auto value = vm -> get_float(1);
+                base_class::get_environment() -> set_ambient_light_sky_contribution(value);
+                vm -> push_value(true);
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "get_reflection_source", [](auto vm, auto& id) -> int {
+                vm -> push_value(static_cast<int>(base_class::get_environment() -> get_reflection_source()));
+                return 1;
+            });
+
+            API::bind(vm, base_scope, "set_reflection_source", [](auto vm, auto& id) -> int {
+                vm_args(vm, id, "(source)")
+                    .require(1, &Machine::is_number)
+                    .validate_enum(1, godot::Environment::REFLECTION_SOURCE_BG, godot::Environment::REFLECTION_SOURCE_SKY);
+
+                auto source = static_cast<godot::Environment::ReflectionSource>(vm -> get_int(1));
+                base_class::get_environment() -> set_reflection_source(source);
                 vm -> push_value(true);
                 return 1;
             });
