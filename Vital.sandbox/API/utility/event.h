bbbf1fb451bef29db1e355670cf8f128f1842e59
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-11 09:27:23 +0300

    Update event.h

--- Vital.sandbox/API/utility/event.h
+++ Vital.sandbox/API/utility/event.h
@@ -76,7 +76,7 @@ namespace Vital::Sandbox::API {
         inline static std::mutex reply_callbacks_mutex;
 
         static void read_config(Machine* vm, int idx, Handler& handler) {
-            if (vm -> get_count() < idx || !vm -> is_table(idx)) return;
+            if ((vm -> get_count() < idx) || !vm -> is_table(idx)) return;
             vm -> table_get_value("async", idx); handler.async = vm -> get_bool(-1); vm -> pop(1);
             vm -> table_get_value("subscription_limit", idx);
             if (vm -> is_number(-1)) handler.subscription_limit = std::max(1, vm -> get_int(-1));
