edab124068a851df7a2b2eab3b9961e58d38413d
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-28 11:46:00 +0300

    Update audio_2d.cpp

--- Vital.sandbox/Engine/private/audio_2d.cpp
+++ Vital.sandbox/Engine/private/audio_2d.cpp
@@ -3,6 +3,7 @@
      Script: Engine: audio_2d.cpp
      Author: ov-studio
      Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
      Desc: Audio 2D Utilities
 ----------------------------------------------------------------*/
 
