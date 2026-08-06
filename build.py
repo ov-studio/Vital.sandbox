6bf689779a8f1ce06c157d16039a5dc451a61310
A <crazynuggets@outlook.com>
2026-08-06 20:48:14 +0300

    Update build.py

--- build.py
+++ build.py
@@ -84,6 +84,7 @@ class Build:
             "use_static_crt=no",
             "build_library=no",
             f"debug_symbols={'yes' if self.build_type == 'Debug' else 'no'}",
+            f"extra_cppdefines={godot_define}",
             f"-j{int(self.os_info['nproc'])}",
         ]
 
