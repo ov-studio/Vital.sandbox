38b3f56e0a42253d1f51d8a31fdff51bf90181bc
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-11 09:48:25 +0300

    Update core.cpp

--- Vital.sandbox/Engine/private/core.cpp
+++ Vital.sandbox/Engine/private/core.cpp
@@ -20,6 +20,7 @@
 #include <Vital.sandbox/Manager/public/asset.h>
 #include <Vital.sandbox/Manager/public/sandbox.h>
 #include <Vital.sandbox/Manager/public/resource.h>
+#include <Vital.sandbox/Manager/public/masterlist.h>
 
 
 //////////////////////////
@@ -139,6 +140,8 @@ namespace Vital::Engine {
         Manager::Resource::get_singleton() -> stop_all();
         enqueue([this]() {
             Tool::print("sbox", "Core: shut down successfully!");
+            // TODO: Use some hook? maybe teardown needs to be called here? but then asset etc should be torn down when disconnect instead of just shutdown...
+            Manager::Masterlist::get_singleton() -> stop();
             Engine::Console::get_singleton() -> teardown();
             std::this_thread::sleep_for(std::chrono::milliseconds(2500));
             free_singleton();
