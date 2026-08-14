56ead032b4346eb6db54ae0b6af2d17c2418e7b5
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-14 20:46:23 +0300

    Disable console toggles until splash is hidden

--- Vital.sandbox/Engine/private/console.cpp
+++ Vital.sandbox/Engine/private/console.cpp
@@ -708,6 +708,7 @@ namespace Vital::Engine {
     void Console::toggle() {
         if (!is_ready()) return;
         const bool state = !is_visible();
+        if (Engine::Splash::has_singleton() && Engine::Splash::get_singleton() -> is_visible() && state) return;
         webview -> set_visible(state);
         if (state) webview -> set_focussed(true);
     }
@@ -732,7 +733,6 @@ namespace Vital::Engine {
 
     bool Console::on_key(int keycode) {
         const auto bind = Manager::Kit::fetch_json_value("config/console", "bind");
-        if (Engine::Splash::has_singleton() && Engine::Splash::get_singleton() -> is_visible()) return false;
         if (keycode != godot::OS::get_singleton() -> find_keycode_from_string(Tool::to_godot_string(bind.as<std::string>()))) return false;
         Engine::Console::get_singleton() -> toggle();
         Engine::Core::get_singleton() -> get_viewport() -> set_input_as_handled();
