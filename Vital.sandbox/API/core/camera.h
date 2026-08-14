e2dad6998539a184da68af2c25e67d6d060d35c2
A <ovanisa5454@gmail.com>
2026-08-14 09:51:58 +0300

    Update camera.h

--- Vital.sandbox/API/core/camera.h
+++ Vital.sandbox/API/core/camera.h
@@ -108,7 +108,7 @@ namespace Vital::Sandbox::API {
         }
 
         static void methods(Machine* vm) {
-            API::Node_3D::methods<Instance>(vm);
+            API::Node_3D::methods<Instance, Node_3D::Type::Spatial>(vm);
 
             vm_module::bind_method<Instance>(vm, "is_active", [](auto vm, auto self, auto& id) -> int {
                 vm -> push_value(self -> camera -> is_active());
