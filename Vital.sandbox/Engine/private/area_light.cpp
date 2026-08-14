ac68caf13e452693c03ce6479d42e4a9964b206f
A <ovanisa5454@gmail.com>
2026-08-14 08:39:09 +0300

    Added area_light wrapper

---
+++ Vital.sandbox/Engine/private/area_light.cpp
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Engine: area_light.cpp
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Area Light Utilities
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#if defined(VSDK_Client)
+#include <Vital.sandbox/Engine/public/area_light.h>
+
+
+////////////////////////////////
+// Vital: Engine: Area_Light //
+////////////////////////////////
+
+namespace Vital::Engine {
+    // Managers //
+    Area_Light* Area_Light::create() {
+        auto light = memnew(Area_Light);
+        Engine::Core::get_scene_root() -> add_child(light);
+        return light;
+    }
+
+    void Area_Light::destroy() {
+        queue_free();
+    }
+}
+#endif
