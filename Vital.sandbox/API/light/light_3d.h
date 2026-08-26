f9532e94ea69f272ba772fa566da8b145d2e0607
A <89867985+ov-aviril@users.noreply.github.com>
2026-08-26 22:46:36 +0300

    Update light_3d.h

--- Vital.sandbox/API/light/light_3d.h
+++ Vital.sandbox/API/light/light_3d.h
@@ -349,7 +349,7 @@ namespace Vital::Sandbox::API {
                 auto ref = path;
                 auto base = API::File::assert_file(vm, path);
                 auto texture = Vital::Engine::Texture::get_from_reference(ref);
-                if (!texture) texture = Vital::Engine::Image::create_texture_2d(base, path, true, ref);
+                if (!texture) texture = Vital::Engine::Image::create(base, path, true, ref);
                 self -> light -> set_projector(texture -> get_texture());
                 vm -> push_value(path);
                 self -> set_reference(self -> value_reference("projector"), -1);
