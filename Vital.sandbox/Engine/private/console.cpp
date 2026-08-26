c58c92e7a962f42d1f2912f31680fe1ec8da050c
A <89867985+ov-aviril@users.noreply.github.com>
2026-08-26 10:02:15 +0300

    Update console.cpp

--- Vital.sandbox/Engine/private/console.cpp
+++ Vital.sandbox/Engine/private/console.cpp
@@ -25,7 +25,6 @@
 // Vital: Engine: Console //
 /////////////////////////////
 
-// TODO: Improve
 namespace Vital::Engine {
     // Helpers //
     std::string Console::Internal::fetch_mode_label(const std::string& mode) {
