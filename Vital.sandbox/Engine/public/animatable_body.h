eb403beffd4f99bbc47e675c91355323d442c7a3
A <89867985+ov-aviril@users.noreply.github.com>
2026-08-29 12:06:26 +0300

    Update animatable_body.h

--- Vital.sandbox/Engine/public/animatable_body.h
+++ Vital.sandbox/Engine/public/animatable_body.h
@@ -15,6 +15,7 @@
 #pragma once
 #if defined(VSDK_Client)
 #include <Vital.sandbox/Engine/public/core.h>
+#include <Vital.sandbox/Engine/public/physics_body.h>
 
 
 /////////////////////////////////////
