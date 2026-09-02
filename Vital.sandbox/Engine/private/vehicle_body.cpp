f9bdfaed58d70f6b83638e0e0d6cf8f5454bfbb6
A <avirilaviril@protonmail.com>
2026-09-02 13:01:32 +0300

    Adapted vehicle body

--- Vital.sandbox/Engine/private/vehicle_body.cpp
+++ Vital.sandbox/Engine/private/vehicle_body.cpp
@@ -21,34 +21,36 @@
 // Vital: Engine: Vehicle_Body //
 //////////////////////////////////
 
-// TODO: Improve
-
 namespace Vital::Engine {
+    // Hooks //
+    void Vehicle_Body::_ready() { 
+        _ready_sync(pending_authority);
+    }
+
+    void Vehicle_Body::_process(double delta) { 
+        on_sync_process(delta);
+    }
+
+    
+    // Managers //
     Vehicle_Body* Vehicle_Body::create(int authority_peer) {
         auto body = memnew(Vehicle_Body);
         #if !defined(VSDK_Client)
-        if (authority_peer != 0) {
-            body->net_id            = body->next_net_id++;
-            body->pending_authority = authority_peer;
-            uint32_t captured_id    = body->net_id;
-            int captured_auth       = authority_peer;
-            Core::get_singleton()->add_child(body);
-            Core::get_singleton()->enqueue([body, captured_id, captured_auth]() {
-                Manager::Network::get_singleton()->enqueue_syncable_registration(body);
-                auto net_node = Manager::Network::get_singleton()->get_node();
-                if (net_node) {
-                    net_node->rpc("_spawn_entity",
-                        (int)captured_id,
-                        (int)ISyncable::SyncType::PhysicsBody,
-                        godot::String("vehicle"),
-                        captured_auth);
-                }
-            });
-        } else {
-            Core::get_singleton()->add_child(body);
-        }
+            if (authority_peer != 0) {
+                body -> net_id = body -> next_net_id++;
+                body -> pending_authority = authority_peer;
+                uint32_t captured_id = body -> net_id;
+                int captured_auth = authority_peer;
+                Core::get_singleton() -> add_child(body);
+                Core::get_singleton() -> enqueue([body, captured_id, captured_auth]() {
+                    Manager::Network::get_singleton() -> enqueue_syncable_registration(body);
+                    auto net_node = Manager::Network::get_singleton() -> get_node();
+                    if (net_node) net_node  ->  rpc("_spawn_entity", (int)captured_id, (int)ISyncable::SyncType::PhysicsBody, godot::String("vehicle"), captured_auth);
+                });
+            }
+            else Core::get_singleton() -> add_child(body);
         #else
-        Core::get_singleton()->add_child(body);
+            Core::get_singleton() -> add_child(body);
         #endif
         return body;
     }
@@ -56,10 +58,16 @@ namespace Vital::Engine {
     void Vehicle_Body::destroy() {
         #if !defined(VSDK_Client)
         if (net_id != 0) {
-            auto net_node = Manager::Network::get_singleton()->get_node();
-            if (net_node) net_node->rpc("_destroy_entity", (int)net_id);
+            auto net_node = Manager::Network::get_singleton() -> get_node();
+            if (net_node) net_node -> rpc("_destroy_entity", (int)net_id);
         }
         #endif
         queue_free();
+    }
+
+
+    // Getters //
+    Engine::PhysicsType get_physics_type() const override { 
+        return PhysicsType::Vehicle; 
     }
 }
