87e5076ba24bb6f9a0fa8c5d58dce8962469bc5f
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-14 13:50:18 +0300

    Update event.cpp

--- Vital.sandbox/Manager/private/event.cpp
+++ Vital.sandbox/Manager/private/event.cpp
@@ -16,8 +16,6 @@
 #include <Vital.sandbox/Vital/entrypoint.h>
 // TODO: Improve / Remove later
 #include <Vital.sandbox/API/core/model.h>
-#include <Vital.sandbox/API/core/webview.h>
-#include <Vital.sandbox/API/core/font.h>
 
 
 /////////////
