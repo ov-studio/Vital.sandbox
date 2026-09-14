e817584f919dbee930df5051017074f27677d35b
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-14 10:56:50 +0300

    Update config.h

--- Vital.sandbox/Vital/config.h
+++ Vital.sandbox/Vital/config.h
@@ -3,7 +3,7 @@
      Script: Vital: config.h
      Author: ov-studio
      Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
-     DOC: 14/09/2026
+     DOC: 14/09/2022
      Desc: Precompiled Config Header
 ----------------------------------------------------------------*/
 
