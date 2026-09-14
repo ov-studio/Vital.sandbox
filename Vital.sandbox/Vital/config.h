3a3ae9cf54e069fd4b7ffbc5af6d3a8af8f24b91
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-14 10:54:56 +0300

    Create config.h

---
+++ Vital.sandbox/Vital/config.h
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: Vital: config.h
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2026
+     Desc: Precompiled Config Header
+----------------------------------------------------------------*/
+
+
+//////////////
+// Imports //
+//////////////
+
+#pragma once
+#if !defined(VSDK_Client)
+#include <Vital.sandbox/Config/server.h>
+#endif
