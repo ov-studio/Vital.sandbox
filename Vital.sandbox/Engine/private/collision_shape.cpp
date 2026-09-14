7a232fbafe97d3e6757337700e281f5f8bb4d52a
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-15 02:18:48 +0300

    Update collision_shape.cpp

--- Vital.sandbox/Engine/private/collision_shape.cpp
+++ Vital.sandbox/Engine/private/collision_shape.cpp
@@ -109,13 +109,9 @@ namespace Vital::Engine {
     }
 
     void Collision_Shape::set_debug_all(bool state) {
-        #if defined(VSDK_Client)
         debug_all = state;
         std::lock_guard<std::mutex> lock(mutex);
         for (auto* shape : live_instances) shape -> set_debug_visible(state);
-        #else
-        (void)state;
-        #endif
     }
     #endif
 
