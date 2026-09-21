9cf55e9aec8e4b527dbcd2c9eab4832e1f45c810
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-21 18:20:34 +0300

    Update instance.h

--- Vital.sandbox/Sandbox/runtime/instance.h
+++ Vital.sandbox/Sandbox/runtime/instance.h
@@ -126,11 +126,8 @@ namespace Vital::Sandbox {
                 std::lock_guard<std::mutex> lock(Derived::Owner::registry.mutex);
                 for (auto it = Derived::Owner::registry.buffer.begin(); it != Derived::Owner::registry.buffer.end();) {
                     auto& instance = it -> second;
-                    if (instance -> get_node() != ptr) { 
-                        ++it; 
-                        continue;
-                    }
                     ++it;
+                    if (instance -> get_node() != ptr) continue;
                     erase_unlocked(instance);
                     Vital::Engine::Core::get_singleton() -> execute([instance, on_release]() {
                         if (on_release) on_release(instance);
