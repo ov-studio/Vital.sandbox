87c322051290d82976d9a22caa5343390d270fee
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-22 09:22:11 +0300

    Update animatable_body.h

--- Vital.sandbox/API/physics/animatable_body.h
+++ Vital.sandbox/API/physics/animatable_body.h
@@ -25,6 +25,7 @@
 namespace Vital::Sandbox::API {
     struct Animatable_Body : vm_module {
         inline static const std::vector<std::string> base_scope = {"physics", "animatable"};
+        inline static constexpr bool has_remote = true;
         using base_class = Vital::Engine::Animatable_Body;
 
         struct Instance : Physics_Body_Instance<Instance, base_class> {
