ee52d881792432bac98d1a443b0003cf2f644341
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-22 10:17:46 +0300

    Update node_3d.h

--- Vital.sandbox/API/base/node_3d.h
+++ Vital.sandbox/API/base/node_3d.h
@@ -66,12 +66,6 @@ namespace Vital::Sandbox::API {
 
         template<typename Instance, Type node_type = Type::Spatial>
         static void methods(Machine* vm) {
-            #if defined(VSDK_Client)
-            vm_module::bind_method<Instance>(vm, "is_streamed", [](auto vm, auto self, auto& id) -> int {
-                vm -> push_value(self -> is_streamed());
-                return 1;
-            });
-            #endif
             if constexpr (node_type == Type::Spatial) {
                 vm_module::bind_method<Instance>(vm, "is_visible", [](auto vm, auto self, auto& id) -> int {
                     vm -> push_value(self -> get_node() -> is_visible());
