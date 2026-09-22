3e245691c7aa7b110613b8dc5303ecaeb3e7b411
A <89867985+ov-aviril@users.noreply.github.com>
2026-09-22 09:18:29 +0300

    Update rigid_body.h

--- Vital.sandbox/API/physics/rigid_body.h
+++ Vital.sandbox/API/physics/rigid_body.h
@@ -25,6 +25,7 @@
 namespace Vital::Sandbox::API {
     struct Rigid_Body : vm_module {
         inline static const std::vector<std::string> base_scope = {"physics", "rigid"};
+        inline static constexpr bool has_remote = true;
         using base_class = Vital::Engine::Rigid_Body;
 
         inline static const std::vector<std::pair<std::string, base_class::FreezeMode>> freeze_mode_registry = {
